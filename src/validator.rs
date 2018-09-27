67e83f7c279422da6d7171ef71370faf53e5a945
Dan Gohman <sunfish@mozilla.com>
2018-09-27 15:13:32 -0700

    [clippy] Pass a slice rather than a &Vec.

--- src/validator.rs
+++ src/validator.rs
@@ -313,7 +313,7 @@ struct OperatorValidator {
 impl OperatorValidator {
     pub fn new(
         func_type: &FuncType,
-        locals: &Vec<(u32, Type)>,
+        locals: &[(u32, Type)],
         config: OperatorValidatorConfig,
     ) -> OperatorValidator {
         let mut local_types = Vec::new();
