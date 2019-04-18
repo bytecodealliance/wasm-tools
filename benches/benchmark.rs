be5959a12682771499e047b54975a06e07d09782
Yury Delendik <ydelendik@mozilla.com>
2019-04-17 19:15:06 -0500

    Fix benchmarks and add validate() benchmark.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -20,14 +20,30 @@ extern crate criterion;
 extern crate wasmparser;
 
 use criterion::Criterion;
-use wasmparser::{OperatorValidatorConfig, Parser, ValidatingParser, ValidatingParserConfig};
+use wasmparser::{
+    validate, OperatorValidatorConfig, Parser, ParserState, ValidatingParser,
+    ValidatingParserConfig, WasmDecoder,
+};
 
 use std::fs::{read_dir, File};
 use std::io::Read;
 use std::path::PathBuf;
 
+fn read_all_wasm<'a, T>(mut d: T)
+where
+    T: WasmDecoder<'a>,
+{
+    loop {
+        match *d.read() {
+            ParserState::Error(e) => panic!("unexpected error {:?}", e),
+            ParserState::EndWasm => return,
+            _ => (),
+        }
+    }
+}
+
 fn it_works_benchmark(c: &mut Criterion) {
-    let mut data: Vec<Vec<u8>> = vec![vec![]];
+    let mut data: Vec<Vec<u8>> = vec![];
     for entry in read_dir("tests").unwrap() {
         let dir = entry.unwrap();
         if !dir.file_type().unwrap().is_file() {
@@ -37,13 +53,13 @@ fn it_works_benchmark(c: &mut Criterion) {
     }
     c.bench_function("it works benchmark", move |b| {
         for d in &mut data {
-            b.iter(|| Parser::new(d.as_slice()))
+            b.iter(|| read_all_wasm(Parser::new(d.as_slice())));
         }
     });
 }
 
 fn validator_not_fails_benchmark(c: &mut Criterion) {
-    let mut data: Vec<Vec<u8>> = vec![vec![]];
+    let mut data: Vec<Vec<u8>> = vec![];
     for entry in read_dir("tests").unwrap() {
         let dir = entry.unwrap();
         if !dir.file_type().unwrap().is_file() {
@@ -53,10 +69,31 @@ fn validator_not_fails_benchmark(c: &mut Criterion) {
     }
     c.bench_function("validator no fails benchmark", move |b| {
         for d in &mut data {
-            b.iter(|| ValidatingParser::new(d.as_slice(), VALIDATOR_CONFIG));
+            b.iter(|| read_all_wasm(ValidatingParser::new(d.as_slice(), VALIDATOR_CONFIG)));
         }
     });
 }
 
-criterion_group!(benchmark, it_works_benchmark, validator_not_fails_benchmark);
+fn validate_benchmark(c: &mut Criterion) {
+    let mut data: Vec<Vec<u8>> = vec![vec![]];
+    for entry in read_dir("tests").unwrap() {
+        let dir = entry.unwrap();
+        if !dir.file_type().unwrap().is_file() {
+            continue;
+        }
+        data.push(read_file_data(&dir.path()));
+    }
+    c.bench_function("validate benchmark", move |b| {
+        for d in &mut data {
+            b.iter(|| validate(&d, VALIDATOR_CONFIG));
+        }
+    });
+}
+
+criterion_group!(
+    benchmark,
+    it_works_benchmark,
+    validator_not_fails_benchmark,
+    validate_benchmark
+);
 criterion_main!(benchmark);
