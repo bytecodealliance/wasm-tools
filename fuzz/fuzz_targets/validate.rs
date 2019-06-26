7703c4f5027fa8626446dcd293299aaa0d1e1a3c
Dan Gohman <sunfish@mozilla.com>
2019-06-26 15:30:21 -0700

    Fix compilation of fuzz_targets/validate.rs.

--- fuzz/fuzz_targets/validate.rs
+++ fuzz/fuzz_targets/validate.rs
@@ -7,7 +7,7 @@ extern crate wasmparser;
 use wasmparser::WasmDecoder;
 
 fuzz_target!(|data: &[u8]| {
-    let mut parser = wasmparser::ValidatingParser::new(data);
+    let mut parser = wasmparser::ValidatingParser::new(data, None);
     loop {
         match *parser.read() {
             wasmparser::ParserState::Error(..) | wasmparser::ParserState::EndWasm => break,
