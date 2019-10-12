8465a6097de059e0ddf01ad2fb35114540559a50
Alex Crichton <alex@alexcrichton.com>
2019-10-11 23:57:31 -0700

    Add a parser fuzz target

---
+++ fuzz/fuzz_targets/parser.rs
@@ -0,0 +1,17 @@
+#![no_main]
+
+use libfuzzer_sys::*;
+use std::str;
+
+fuzz_target!(|data: &[u8]| {
+    let s = match str::from_utf8(data) {
+        Ok(s) => s,
+        Err(_) => return,
+    };
+    let buf = match wast::parser::ParseBuffer::new(s) {
+        Ok(b) => b,
+        Err(_) => return,
+    };
+    drop(buf.parser().parse::<wast::ast::File>());
+});
+
