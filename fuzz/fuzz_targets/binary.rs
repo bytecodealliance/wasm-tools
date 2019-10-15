bf1e111c4b800f8187fce4f6e6b82855d4f66015
Alex Crichton <alex@alexcrichton.com>
2019-10-15 16:35:41 -0700

    Add skeleton of `wat2wasm` checking in binary fuzz
    Disable it for now though due to #8

--- fuzz/fuzz_targets/binary.rs
+++ fuzz/fuzz_targets/binary.rs
@@ -1,11 +1,37 @@
 #![no_main]
 
 use libfuzzer_sys::*;
+use std::process::Command;
 use std::str;
 
 fuzz_target!(|data: &[u8]| {
-    match str::from_utf8(data) {
-        Ok(s) => drop(wast::parse_str(s)),
-        Err(_) => {}
+    let s = match str::from_utf8(data) {
+        Ok(s) => s,
+        Err(_) => return,
+    };
+    let binary = match wast::parse_str(s) {
+        Ok(bytes) => bytes,
+        Err(_) => return,
+    };
+
+
+    // FIXME(#8) needs some more work to enable
+    if true {
+        return;
+    }
+
+    let td = tempfile::TempDir::new().unwrap();
+    let wat = td.path().join("foo.wat");
+    let wasm = td.path().join("foo.wasm");
+    std::fs::write(&wat, &s).unwrap();
+    let output = Command::new("wat2wasm")
+        .arg(&wat)
+        .arg("-o")
+        .arg(&wasm)
+        .output()
+        .unwrap();
+    if output.status.success() {
+        let wabt_bytes = std::fs::read(&wasm).unwrap();
+        assert_eq!(binary, wabt_bytes);
     }
 });
