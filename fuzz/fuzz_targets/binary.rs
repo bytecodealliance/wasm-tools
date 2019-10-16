52733acece7c39473844fb77a8d8521af43fdd3c
Alex Crichton <alex@alexcrichton.com>
2019-10-16 15:07:59 -0700

    Perform differential fuzzing against `wat2wasm`
    Ensure that if we fail, wat2wasm fails. Additionally if we succeed, in
    most cases assert that `wat2wasm` produces the exact same binary.
    
    Closes #8

--- fuzz/fuzz_targets/binary.rs
+++ fuzz/fuzz_targets/binary.rs
@@ -9,28 +9,78 @@ fuzz_target!(|data: &[u8]| {
         Ok(s) => s,
         Err(_) => return,
     };
-    let binary = match wast::parse_str(s) {
-        Ok(bytes) => bytes,
-        Err(_) => return,
-    };
-
-    // FIXME(#8) needs some more work to enable
-    if true {
-        return;
-    }
 
     let td = tempfile::TempDir::new().unwrap();
     let wat = td.path().join("foo.wat");
     let wasm = td.path().join("foo.wasm");
     std::fs::write(&wat, &s).unwrap();
-    let output = Command::new("wat2wasm")
-        .arg(&wat)
-        .arg("-o")
-        .arg(&wasm)
-        .output()
-        .unwrap();
-    if output.status.success() {
-        let wabt_bytes = std::fs::read(&wasm).unwrap();
-        assert_eq!(binary, wabt_bytes);
+
+    match wast::parse_str(s) {
+        // If we succesfully parsed the binary, then we want to make sure that
+        // `wabt` agrees on the binary encoding of the input wat file. There's a
+        // few caveats though:
+        //
+        // * We ignore `(module binary ...)` modules since wabt postprocesses
+        //   those but we don't
+        //
+        // * We ignore anything with a hex float literal. Our parsing is
+        //   different than wabt's (#13) and it's not super interesting for us
+        //   to fuzz those differences right now anyway.
+        //
+        // * Wabt looks to have lots of various subtle issues around non-MVP
+        //   features. For now lets just fuzz MVP features by avoiding passing
+        //   `--enable-all`, and then only if `wat2wasm` succeeds to we compare
+        //   the binary.
+        //
+        // Ideally we'd just unconditionally pass everything to `wat2wasm` and
+        // verify that it's the same, but for now that's producing a lot of
+        // unintersting failures.
+        Ok(binary) => {
+            let lexer = wast_parser::lexer::Lexer::new(s);
+            for token in lexer {
+                let t = match token.unwrap() {
+                    wast_parser::lexer::Source::Token(t) => t,
+                    _ => continue,
+                };
+                match t {
+                    wast_parser::lexer::Token::Keyword(k) => {
+                        if k == "binary" {
+                            return;
+                        }
+                    }
+                    wast_parser::lexer::Token::Float(f) => {
+                        if let wast_parser::lexer::FloatVal::Val { hex: true, .. } = f.val() {
+                            return;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            let output = Command::new("wat2wasm")
+                .arg(&wat)
+                .arg("-o")
+                .arg(&wasm)
+                .output()
+                .unwrap();
+            if output.status.success() {
+                let wabt_bytes = std::fs::read(&wasm).unwrap();
+                assert_eq!(binary, wabt_bytes);
+            }
+        }
+
+        // If we failed to parse the input wasm, then `wat2wasm` from `wabt`
+        // should fail as well.
+        Err(_) => {
+            let output = Command::new("wat2wasm")
+                .arg(&wat)
+                .arg("--no-check")
+                .arg("--enable-all")
+                .arg("-o")
+                .arg(&wasm)
+                .output()
+                .unwrap();
+            assert!(!output.status.success());
+        }
     }
 });
