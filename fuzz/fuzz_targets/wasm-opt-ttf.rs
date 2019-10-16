bcf1b86e95929fa3a5c400c72a119f31bd97777d
Alex Crichton <alex@alexcrichton.com>
2019-10-16 15:11:25 -0700

    Add fuzzing with `wasm-opt -ttf`
    Closes #7

---
+++ fuzz/fuzz_targets/wasm-opt-ttf.rs
@@ -0,0 +1,67 @@
+//! Same as `binary.rs` fuzzing, but use `wasm-opt` with the input data to
+//! generate a wasm program using `-ttf` instead of interpreting `data` as a
+//! wasm program.
+//!
+//! Additionally assert that parsing succeeds since we should be able to parse
+//! everything `wasm-opt` generates.
+
+#![no_main]
+
+use libfuzzer_sys::*;
+use std::io::{Read, Write};
+use std::process::{Command, Stdio};
+
+fuzz_target!(|data: &[u8]| {
+    let td = tempfile::TempDir::new().unwrap();
+    std::fs::write(td.path().join("input"), data).unwrap();
+    let mut cmd = Command::new("wasm-opt")
+        .arg("-ttf")
+        .arg("--emit-text")
+        .arg(td.path().join("input"))
+        .arg("-o")
+        .arg("-")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    cmd.stdin.take().unwrap().write_all(data).unwrap();
+    let mut s = String::new();
+    cmd.stdout.take().unwrap().read_to_string(&mut s).unwrap();
+
+    let wat = td.path().join("foo.wat");
+    let wasm = td.path().join("foo.wasm");
+    std::fs::write(&wat, &s).unwrap();
+
+    let binary = wast::parse_str(&s).unwrap();
+    let lexer = wast_parser::lexer::Lexer::new(&s);
+    for token in lexer {
+        let t = match token.unwrap() {
+            wast_parser::lexer::Source::Token(t) => t,
+            _ => continue,
+        };
+        match t {
+            wast_parser::lexer::Token::Keyword(k) => {
+                if k == "binary" {
+                    return;
+                }
+            }
+            wast_parser::lexer::Token::Float(f) => {
+                if let wast_parser::lexer::FloatVal::Val { hex: true, .. } = f.val() {
+                    return;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    let output = Command::new("wat2wasm")
+        .arg(&wat)
+        .arg("-o")
+        .arg(&wasm)
+        .output()
+        .unwrap();
+    if output.status.success() {
+        let wabt_bytes = std::fs::read(&wasm).unwrap();
+        assert_eq!(binary, wabt_bytes);
+    }
+});
