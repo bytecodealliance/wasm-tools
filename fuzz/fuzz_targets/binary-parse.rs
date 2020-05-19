773b2fde0a1ecf4826a93030bab683c336bec1e3
Alex Crichton <alex@alexcrichton.com>
2020-05-19 11:07:20 -0700

    Consolidate fuzzers at the top-level

--- fuzz/fuzz_targets/binary-parse.rs
+++ fuzz/fuzz_targets/binary-parse.rs
@@ -1,13 +1,10 @@
 #![no_main]
 
-#[macro_use]
-extern crate libfuzzer_sys;
-extern crate wasmparser;
-
+use libfuzzer_sys::*;
 use wasmparser::WasmDecoder;
 
 fuzz_target!(|data: &[u8]| {
-    let mut parser = wasmparser::ValidatingParser::new(data, None);
+    let mut parser = wasmparser::Parser::new(data);
     loop {
         match *parser.read() {
             wasmparser::ParserState::Error(..) | wasmparser::ParserState::EndWasm => break,
