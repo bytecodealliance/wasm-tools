b637e8912ca2382742deebe324a5eed2250ed274
Hero Bird <robbepop@web.de>
2020-06-05 00:06:06 +0200

    Try to fix wasmparser benchmarks (#24)
    * [wasmparser] add [[bench]] section to Cargo.toml
    
    * [wasmparser] fix benchmark .wast file paths after repo move
    
    * [wasmparser] add missing enable_tail_call to validating parser config
    
    * [wasmparser] reorganize benchmark imports
    
    * [wasmparser] improve error message when reading Wasm benchmarks
    
    * [wasmparser] remove code dupe in benchmarks

--- crates/wasmparser/benches/benchmark.rs
+++ crates/wasmparser/benches/benchmark.rs
@@ -1,9 +1,15 @@
-pub fn read_file_data(path: &PathBuf) -> Vec<u8> {
-    let mut data = Vec::new();
-    let mut f = File::open(path).ok().unwrap();
-    f.read_to_end(&mut data).unwrap();
-    data
-}
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use std::fs::{read_dir, File};
+use std::io::Read;
+use std::path::Path;
+use std::path::PathBuf;
+use wasmparser::{
+    validate, OperatorValidatorConfig, Parser, ParserState, ValidatingParser,
+    ValidatingParserConfig, WasmDecoder,
+};
 
 const VALIDATOR_CONFIG: Option<ValidatingParserConfig> = Some(ValidatingParserConfig {
     operator_config: OperatorValidatorConfig {
@@ -12,22 +18,9 @@ const VALIDATOR_CONFIG: Option<ValidatingParserConfig> = Some(ValidatingParserCo
         enable_simd: true,
         enable_bulk_memory: true,
         enable_multi_value: true,
+        enable_tail_call: true,
     },
 });
-
-#[macro_use]
-extern crate criterion;
-extern crate wasmparser;
-
-use criterion::Criterion;
-use wasmparser::{
-    validate, OperatorValidatorConfig, Parser, ParserState, ValidatingParser,
-    ValidatingParserConfig, WasmDecoder,
-};
-
-use std::fs::{read_dir, File};
-use std::io::Read;
-use std::path::PathBuf;
 
 fn read_all_wasm<'a, T>(mut d: T)
 where
@@ -35,22 +28,37 @@ where
 {
     loop {
         match *d.read() {
-            ParserState::Error(ref e) => panic!("unexpected error {}", e),
+            ParserState::Error(ref e) => panic!("unexpected error while reading Wasm: {}", e),
             ParserState::EndWasm => return,
             _ => (),
         }
     }
 }
 
-fn it_works_benchmark(c: &mut Criterion) {
-    let mut data: Vec<Vec<u8>> = vec![];
-    for entry in read_dir("tests").unwrap() {
+fn read_file_data(path: &PathBuf) -> Vec<u8> {
+    let mut data = Vec::new();
+    let mut f = File::open(path).ok().unwrap();
+    f.read_to_end(&mut data).unwrap();
+    data
+}
+
+fn collect_test_files<P>(path: P) -> Vec<Vec<u8>>
+where
+    P: AsRef<Path>,
+{
+    let mut file_contents: Vec<Vec<u8>> = vec![];
+    for entry in read_dir(path).expect("cannot find the benchmark test files") {
         let dir = entry.unwrap();
         if !dir.file_type().unwrap().is_file() {
             continue;
         }
-        data.push(read_file_data(&dir.path()));
+        file_contents.push(read_file_data(&dir.path()));
     }
+    file_contents
+}
+
+fn it_works_benchmark(c: &mut Criterion) {
+    let mut data = collect_test_files("../../tests");
     c.bench_function("it works benchmark", move |b| {
         for d in &mut data {
             b.iter(|| read_all_wasm(Parser::new(d.as_slice())));
@@ -59,14 +67,7 @@ fn it_works_benchmark(c: &mut Criterion) {
 }
 
 fn validator_not_fails_benchmark(c: &mut Criterion) {
-    let mut data: Vec<Vec<u8>> = vec![];
-    for entry in read_dir("tests").unwrap() {
-        let dir = entry.unwrap();
-        if !dir.file_type().unwrap().is_file() {
-            continue;
-        }
-        data.push(read_file_data(&dir.path()));
-    }
+    let mut data = collect_test_files("../../tests");
     c.bench_function("validator no fails benchmark", move |b| {
         for d in &mut data {
             b.iter(|| read_all_wasm(ValidatingParser::new(d.as_slice(), VALIDATOR_CONFIG)));
@@ -75,14 +76,7 @@ fn validator_not_fails_benchmark(c: &mut Criterion) {
 }
 
 fn validate_benchmark(c: &mut Criterion) {
-    let mut data: Vec<Vec<u8>> = vec![vec![]];
-    for entry in read_dir("tests").unwrap() {
-        let dir = entry.unwrap();
-        if !dir.file_type().unwrap().is_file() {
-            continue;
-        }
-        data.push(read_file_data(&dir.path()));
-    }
+    let mut data = collect_test_files("../../tests");
     c.bench_function("validate benchmark", move |b| {
         for d in &mut data {
             b.iter(|| validate(&d, VALIDATOR_CONFIG));
