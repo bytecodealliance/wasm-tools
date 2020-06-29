1f59e0e75df2646163d2a8defbcca556f0ccd8dc
Andrew Brown <andrew.brown@intel.com>
2020-06-29 13:11:17 -0700

    Bump wasmparser to 0.58.0 (#36)
    * Bump wasmparser to 0.58.0
    
    * Don't use cargo-critcmp which broke recently
    
    Co-authored-by: Alex Crichton <alex@alexcrichton.com>

--- crates/wasmparser/compare-with-main.sh
+++ crates/wasmparser/compare-with-main.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# record current bench results
-cargo bench --bench benchmark -- --noplot --save-baseline after
-
 # switch to main branch and record its bench results
 git checkout main && \
 cargo bench --bench benchmark -- --noplot --save-baseline before
 
-# compare
-cargo install critcmp --force && \
-critcmp before after
+# record current bench results
+git checkout - && \
+cargo bench --bench benchmark -- --noplot --baseline before
