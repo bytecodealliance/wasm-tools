0d6b433499536f6c3f080ee714f7f0a3c2fe556f
Yury Delendik <ydelendik@mozilla.com>
2020-09-25 12:38:28 -0500

    make multi-memory test

--- crates/wasmemit/tests/replace_memory.rs
+++ crates/wasmemit/tests/replace_memory.rs
@@ -17,20 +17,20 @@ macro_rules! assert_wasm_eq {
 fn test_replace_memory() -> Result<()> {
     const TEST: &[u8] = br#"(module
     (memory 1 10)
+    (memory 1 20)
     (func
         i32.const 42 
-        i32.store 0
-    ))
-  "#;
+        i32.store 0 offset=0
+    ))"#;
     let origin_wasm = wat::parse_bytes(TEST)?;
 
     const EXPECTED: &[u8] = br#"(module
-    (import "env" "memory" (memory 1 10))
+    (import "env" "memory" (memory 1 20))
+    (memory 1 10)
     (func
         i32.const 42 
-        i32.store 0
-    ))
-  "#;
+        i32.store 1 offset=0
+    ))"#;
     let expected_wasm = wat::parse_bytes(EXPECTED)?;
 
     let mut module = read(&origin_wasm)?;
@@ -40,20 +40,41 @@ fn test_replace_memory() -> Result<()> {
             kind: ModuleKind::Text(ref mut fields),
             ..
         } => {
-            for f in fields.iter_mut() {
-                if let ModuleField::Memory(mem) = f {
-                    match mem.kind {
-                        MemoryKind::Normal(ty) => {
-                            let import = InlineImport {
-                                module: "env",
-                                field: Some("memory"),
-                            };
-                            mem.kind = MemoryKind::Import { ty, import };
-                        }
-                        _ => bail!("unexpected mem kind"),
+            let (i, f) = fields
+                .iter_mut()
+                .enumerate()
+                .find(|(_, f)| {
+                    if let ModuleField::Memory(Memory {
+                        kind:
+                            MemoryKind::Normal(MemoryType::B32 {
+                                limits: Limits { max: Some(20), .. },
+                                ..
+                            }),
+                        ..
+                    }) = f
+                    {
+                        true
+                    } else {
+                        false
                     }
+                })
+                .unwrap();
+            // replace memory to be import
+            if let ModuleField::Memory(mem) = f {
+                match mem.kind {
+                    MemoryKind::Normal(ty) => {
+                        let import = InlineImport {
+                            module: "env",
+                            field: Some("memory"),
+                        };
+                        mem.kind = MemoryKind::Import { ty, import };
+                    }
+                    _ => bail!("unexpected mem kind"),
                 }
             }
+            // also move it before non-import
+            let m = fields.remove(i);
+            fields.insert(i - 1, m);
         }
         _ => bail!("expected text"),
     }
