2c3b82899fe9a3384b02d59a17bf04435dc31cb5
Nick Fitzgerald <fitzgen@gmail.com>
2020-10-21 10:54:43 -0700

    wasm-smith: add some smoke unit tests
    Previously, `wasm-smith` was tested by fuzzing for 5 minutes in CI, but after
    the merge into the `wasm-tools` repo, it doesn't make sense to do that anymore
    because (a) added CI time, and (b) OSS-fuzz will run our fuzz targets
    continuously anyways.
    
    Instead, here are a few smoke tests to ensure that we generate valid
    modules. These are just intended for local development and a quick double check
    in CI.

---
+++ crates/wasm-smith/tests/tests.rs
@@ -0,0 +1,74 @@
+use arbitrary::{Arbitrary, Unstructured};
+use rand::{rngs::SmallRng, RngCore, SeedableRng};
+use wasm_smith::{ConfiguredModule, Module, SwarmConfig};
+use wasmparser::{Validator, WasmFeatures};
+
+#[test]
+fn smoke_test_module() {
+    let mut rng = SmallRng::seed_from_u64(0);
+    let mut buf = vec![0; 1024];
+    for _ in 0..1024 {
+        rng.fill_bytes(&mut buf);
+        let u = Unstructured::new(&buf);
+        if let Ok(module) = Module::arbitrary_take_rest(u) {
+            let wasm_bytes = module.to_bytes();
+
+            let mut validator = Validator::new();
+            validator.wasm_features(WasmFeatures {
+                multi_value: true,
+                multi_memory: true,
+                bulk_memory: true,
+                ..WasmFeatures::default()
+            });
+
+            assert!(validator.validate_all(&wasm_bytes).is_ok());
+        }
+    }
+}
+
+#[test]
+fn smoke_test_ensure_termination() {
+    let mut rng = SmallRng::seed_from_u64(0);
+    let mut buf = vec![0; 1024];
+    for _ in 0..1024 {
+        rng.fill_bytes(&mut buf);
+        let u = Unstructured::new(&buf);
+        if let Ok(mut module) = Module::arbitrary_take_rest(u) {
+            module.ensure_termination(10);
+            let wasm_bytes = module.to_bytes();
+
+            let mut validator = Validator::new();
+            validator.wasm_features(WasmFeatures {
+                multi_value: true,
+                multi_memory: true,
+                bulk_memory: true,
+                ..WasmFeatures::default()
+            });
+
+            assert!(validator.validate_all(&wasm_bytes).is_ok());
+        }
+    }
+}
+
+#[test]
+fn smoke_test_swarm_config() {
+    let mut rng = SmallRng::seed_from_u64(0);
+    let mut buf = vec![0; 1024];
+    for _ in 0..1024 {
+        rng.fill_bytes(&mut buf);
+        let u = Unstructured::new(&buf);
+        if let Ok(module) = ConfiguredModule::<SwarmConfig>::arbitrary_take_rest(u) {
+            let wasm_bytes = module.to_bytes();
+
+            let mut validator = Validator::new();
+            validator.wasm_features(WasmFeatures {
+                multi_value: true,
+                multi_memory: true,
+                bulk_memory: true,
+                ..WasmFeatures::default()
+            });
+
+            assert!(validator.validate_all(&wasm_bytes).is_ok());
+        }
+    }
+}
