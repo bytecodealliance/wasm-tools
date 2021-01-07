4d4fe713a9c457c88fc59b0b5eccc8748cc87ff8
Alex Crichton <alex@alexcrichton.com>
2021-01-07 08:19:45 -0800

    Improve diagnostics on wasm-smith test failures

--- crates/wasm-smith/tests/tests.rs
+++ crates/wasm-smith/tests/tests.rs
@@ -25,7 +25,7 @@ fn smoke_test_module() {
 
             let mut validator = Validator::new();
             validator.wasm_features(wasm_features());
-            assert!(validator.validate_all(&wasm_bytes).is_ok());
+            validate(&mut validator, &wasm_bytes);
         }
     }
 }
@@ -43,7 +43,7 @@ fn smoke_test_ensure_termination() {
 
             let mut validator = Validator::new();
             validator.wasm_features(wasm_features());
-            assert!(validator.validate_all(&wasm_bytes).is_ok());
+            validate(&mut validator, &wasm_bytes);
         }
     }
 }
@@ -62,7 +62,19 @@ fn smoke_test_swarm_config() {
             let mut features = wasm_features();
             features.module_linking = module.config().module_linking_enabled();
             validator.wasm_features(features);
-            assert!(validator.validate_all(&wasm_bytes).is_ok());
+            validate(&mut validator, &wasm_bytes);
         }
     }
+}
+
+fn validate(validator: &mut Validator, bytes: &[u8]) {
+    let err = match validator.validate_all(bytes) {
+        Ok(()) => return,
+        Err(e) => e,
+    };
+    drop(std::fs::write("test.wasm", &bytes));
+    if let Ok(text) = wasmprinter::print_bytes(bytes) {
+        drop(std::fs::write("test.wat", &text));
+    }
+    panic!("wasm failed to validate {:?}", err);
 }
