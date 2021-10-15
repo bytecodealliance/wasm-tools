b417c1ec0af435acd540cf8b9139c48344621638
Jacarte <javierca@kth.se>
2021-10-15 15:33:46 +0200

    FIX: extend operator types

--- crates/wasm-mutate/src/mutators/peephole/eggsy/encoder.rs
+++ crates/wasm-mutate/src/mutators/peephole/eggsy/encoder.rs
@@ -93,8 +93,11 @@ macro_rules! eterm_operator_2_wasm {
                             None => unreachable!("Extend operation with no type information")
                         }
                     }
-                    Lang::ExtendI32U(_) | Lang::Extend32S(_) | Lang::ExtendI32S(_)=> {
+                    Lang::ExtendI32U(_) | Lang::ExtendI32S(_) => {
                         Some(PrimitiveTypeInfo::I32)
+                    } 
+                    Lang::Extend32S(_) => {
+                        Some(PrimitiveTypeInfo::I64)
                     }
                     Lang::Call(operands) => {
                         $index_at_parent.and_then(|idx|{
