fc5e73d578dfabd124512934b89ab9e162e66edb
Nick Fitzgerald <fitzgen@gmail.com>
2021-10-19 10:27:20 -0700

    Clean up the `mutate` fuzz target

--- fuzz/fuzz_targets/mutate.rs
+++ fuzz/fuzz_targets/mutate.rs
@@ -7,11 +7,22 @@ use wasmparser::WasmFeatures;
 static NUM_RUNS: AtomicU64 = AtomicU64::new(0);
 static NUM_SUCCESSFUL_MUTATIONS: AtomicU64 = AtomicU64::new(0);
 
-fuzz_target!(|inputs: (wasm_smith::Module, u64)| {
+fuzz_target!(|bytes: &[u8]| {
     let _ = env_logger::try_init();
 
+    let mut seed = 0;
+    let (wasm, _config) = match wasm_tools_fuzz::generate_valid_module(bytes, |_config, u| {
+        seed = u.arbitrary()?;
+        Ok(())
+    }) {
+        Ok(m) => m,
+        Err(_) => return,
+    };
+
+    log::debug!("seed = {}", seed);
+
     let old_num_runs = NUM_RUNS.fetch_add(1, Ordering::Relaxed);
-    if old_num_runs % 4096 == 0 && log::log_enabled!(log::Level::Info) {
+    if old_num_runs % 4096 == 4095 && log::log_enabled!(log::Level::Info) {
         let successful = NUM_SUCCESSFUL_MUTATIONS.load(Ordering::Relaxed);
         let percent = successful as f64 / old_num_runs as f64 * 100.0;
         log::info!(
@@ -20,18 +31,6 @@ fuzz_target!(|inputs: (wasm_smith::Module, u64)| {
             old_num_runs,
             percent
         );
-    }
-
-    let (wasm, seed) = inputs;
-    log::debug!("seed = {}", seed);
-
-    let wasm = wasm.to_bytes();
-    if log::log_enabled!(log::Level::Debug) {
-        log::debug!("writing input Wasm to `test.wasm`");
-        std::fs::write("test.wasm", &wasm).expect("should write `test.wasm` okay");
-        log::debug!("writing wat disassembly to `test.wat`");
-        let wat = wasmprinter::print_bytes(&wasm).expect("should disassemble Wasm okay");
-        std::fs::write("test.wat", &wat).expect("should write `test.wat` okay");
     }
 
     let mutated_wasm = wasm_mutate::WasmMutate::default()
@@ -49,20 +48,19 @@ fuzz_target!(|inputs: (wasm_smith::Module, u64)| {
         },
     };
 
-    // Add valiation with all features enabled, like multiple value return
-    let mut features = WasmFeatures::default();
-    // Add other if needed
-    features.multi_value = true;
-
-    let mut validator = wasmparser::Validator::new();
-    validator.wasm_features(features);
-    let validation_result = validator.validate_all(&mutated_wasm);
-    log::debug!("validation result = {:?}", validation_result);
     if log::log_enabled!(log::Level::Debug) {
-        let mutated_text = wasmprinter::print_bytes(mutated_wasm).unwrap();
-        std::fs::write("mutated.wat", &mutated_text).expect("should write `mutated.wat` okay");
+        std::fs::write("mutated.wasm", &mutated_wasm).expect("should write `mutated.wasm` okay");
+        if let Ok(mutated_wat) = wasmprinter::print_bytes(&mutated_wasm) {
+            std::fs::write("mutated.wat", &mutated_wat).expect("should write `mutated.wat` okay");
+        }
     }
 
+    let features = WasmFeatures::default();
+    let mut validator = wasmparser::Validator::new();
+    validator.wasm_features(features);
+
+    let validation_result = validator.validate_all(&mutated_wasm);
+    log::debug!("validation result = {:?}", validation_result);
     assert!(
         validation_result.is_ok(),
         "`wasm-mutate` should always produce a valid Wasm file"
