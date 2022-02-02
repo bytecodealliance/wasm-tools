0dc45ba59f7e2f6e0b105369d0f9b46f7f865622
Peter Huene <phuene@fastly.com>
2022-02-02 12:30:17 -0800

    Make the module and component sections scalar.
    This commit changes the module and component sections from a vector to a single
    scalar module/component, per the current component model binary AST.

--- crates/wasm-encoder/src/components.rs
+++ crates/wasm-encoder/src/components.rs
@@ -7,48 +7,13 @@ use crate::{encoders, Component, ComponentSection, ComponentSectionId};
 /// ```rust
 /// use wasm_encoder::{Component, NestedComponentSection};
 ///
-/// let mut components = NestedComponentSection::new();
-/// components.component(&Component::new());
-///
 /// let mut component = Component::new();
-/// component.section(&components);
+/// component.section(&NestedComponentSection(Component::new()));
 ///
 /// let bytes = component.finish();
 /// ```
 #[derive(Clone, Debug, Default)]
-pub struct NestedComponentSection {
-    bytes: Vec<u8>,
-    num_added: u32,
-}
-
-impl NestedComponentSection {
-    /// Create a new component section encoder.
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    /// The number of components in the section.
-    pub fn len(&self) -> u32 {
-        self.num_added
-    }
-
-    /// Determines if the section is empty.
-    pub fn is_empty(&self) -> bool {
-        self.num_added == 0
-    }
-
-    /// Writes a component into this component section.
-    ///
-    /// The given component must be fully formed.
-    pub fn component(&mut self, component: &Component) -> &mut Self {
-        self.bytes.extend(
-            encoders::u32(u32::try_from(component.bytes.len()).unwrap())
-                .chain(component.bytes.iter().copied()),
-        );
-        self.num_added += 1;
-        self
-    }
-}
+pub struct NestedComponentSection(pub Component);
 
 impl ComponentSection for NestedComponentSection {
     fn id(&self) -> u8 {
@@ -59,12 +24,9 @@ impl ComponentSection for NestedComponentSection {
     where
         S: Extend<u8>,
     {
-        let num_added = encoders::u32(self.num_added);
-        let n = num_added.len();
         sink.extend(
-            encoders::u32(u32::try_from(n + self.bytes.len()).unwrap())
-                .chain(num_added)
-                .chain(self.bytes.iter().copied()),
+            encoders::u32(u32::try_from(self.0.bytes.len()).unwrap())
+                .chain(self.0.bytes.iter().copied()),
         );
     }
 }
--- crates/wasm-encoder/src/modules.rs
+++ crates/wasm-encoder/src/modules.rs
@@ -7,48 +7,13 @@ use crate::{encoders, ComponentSection, ComponentSectionId, Module};
 /// ```rust
 /// use wasm_encoder::{Module, Component, ModuleSection};
 ///
-/// let mut modules = ModuleSection::new();
-/// modules.module(&Module::new());
-///
 /// let mut component = Component::new();
-/// component.section(&modules);
+/// component.section(&ModuleSection(Module::new()));
 ///
 /// let bytes = component.finish();
 /// ```
 #[derive(Clone, Debug, Default)]
-pub struct ModuleSection {
-    bytes: Vec<u8>,
-    num_added: u32,
-}
-
-impl ModuleSection {
-    /// Create a new module section encoder.
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    /// The number of modules in the section.
-    pub fn len(&self) -> u32 {
-        self.num_added
-    }
-
-    /// Determines if the section is empty.
-    pub fn is_empty(&self) -> bool {
-        self.num_added == 0
-    }
-
-    /// Writes a module into this module section.
-    ///
-    /// The given module must be fully formed.
-    pub fn module(&mut self, module: &Module) -> &mut Self {
-        self.bytes.extend(
-            encoders::u32(u32::try_from(module.bytes.len()).unwrap())
-                .chain(module.bytes.iter().copied()),
-        );
-        self.num_added += 1;
-        self
-    }
-}
+pub struct ModuleSection(pub Module);
 
 impl ComponentSection for ModuleSection {
     fn id(&self) -> u8 {
@@ -59,12 +24,9 @@ impl ComponentSection for ModuleSection {
     where
         S: Extend<u8>,
     {
-        let num_added = encoders::u32(self.num_added);
-        let n = num_added.len();
         sink.extend(
-            encoders::u32(u32::try_from(n + self.bytes.len()).unwrap())
-                .chain(num_added)
-                .chain(self.bytes.iter().copied()),
+            encoders::u32(u32::try_from(self.0.bytes.len()).unwrap())
+                .chain(self.0.bytes.iter().copied()),
         );
     }
 }
