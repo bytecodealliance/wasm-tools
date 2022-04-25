d4462048bd3538d7b07df9447b841985e0839f74
Dan Gohman <dev@sunfishcode.online>
2022-04-25 12:18:37 -0700

    Put components support behind a cargo feature.

--- crates/wast/src/ast/wat.rs
+++ crates/wast/src/ast/wat.rs
@@ -40,7 +40,7 @@ impl<'a> Parse<'a> for Wat<'a> {
         let _r = parser.register_annotation("custom");
         let wat = if parser.peek2::<kw::module>() {
             Wat::Module(parser.parens(|parser| parser.parse())?)
-        } else if parser.peek2::<kw::component>() {
+        } else if cfg!(feature = "components") && parser.peek2::<kw::component>() {
             Wat::Component(parser.parens(|parser| parser.parse())?)
         } else {
             let fields = ModuleField::parse_remaining(parser)?;
