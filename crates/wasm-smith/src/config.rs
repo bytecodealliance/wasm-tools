ac8eb1acab1d83f70c0b488b5c28b79a08d80eb8
Andrew Brown <andrew.brown@intel.com>
2022-06-08 11:36:58 -0700

    review: disable threads in 'SwarmConfig'

--- crates/wasm-smith/src/config.rs
+++ crates/wasm-smith/src/config.rs
@@ -532,7 +532,6 @@ impl<'a> Arbitrary<'a> for SwarmConfig {
             max_nesting_depth: u.int_in_range(0..=10)?,
             saturating_float_to_int_enabled: u.arbitrary()?,
             sign_extension_enabled: u.arbitrary()?,
-            threads_enabled: u.arbitrary()?,
 
             // These fields, unlike the ones above, are less useful to set.
             // They either make weird inputs or are for features not widely
@@ -561,6 +560,7 @@ impl<'a> Arbitrary<'a> for SwarmConfig {
             max_type_size: 1000,
             canonicalize_nans: false,
             available_imports: None,
+            threads_enabled: false,
         })
     }
 }
