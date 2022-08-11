bytecodealliance/wasm-tools

commit 5cb745233fb1a57541816e88b00762c0696fc663

`wasmparser`: VisitOperator API (#697)
* add VisitOperator trait

The trait method identifiers have been automatically generated using the `heck` crate.
This is the same that I would have used if I were to generate the trait using a proc. macro.
A proc. macro was not used since I wanted to keep the PR as simple and contained as possible.

I figured all those trait methods do not necessarily need docs since that would have been very daunting work to add.
Also docs for all the trait methods can be added later.

One downside of the non-generated trait is that we are going to have to keep those identifiers in sync manually.

* implement BinaryReader::visit_operator method

Note: This was primarily copy pasted from the respective match construct used so far.

* implement VisitOperator for OperatorValidator

This was the biggest chunk of the work and it was also not trivial to copy over from the `match` based code constructs.
I tried to deduplicate most of the same validation logic by splitting out those parts into their own functions such as OperatorValidator::check_v128_shift_op etc.

We might want to use these VisitOperator impl in the respective `match` based impementation to deduplicate validation logic.
This refactoring should be simple since we will only forward to the respective visitor based method.
The minor downside is that we can no longer merge match arms while forwarding.

* implement VisitOperator for FuncValidator

This was relatively straight forward and most of the work could be automated.
This basically forwards all the methods to the underlying OperatorValidator impls and adds offsets to respective errors as does the original implementation.
I prevent rustfmt from doing its job here so that we can do bulk line based transformations of the code.

* add OperatorsReader::visit_with_offset method

This is the API to the new visitor based operator validation.

* apply rustfmt

crates/wasmparser/src/validator/core.rs
@@ -8,7 +8,7 @@ use super::{
 use crate::{
     limits::*, BinaryReaderError, ConstExpr, Data, DataKind, Element, ElementItem, ElementKind,
     ExternalKind, FuncType, Global, GlobalType, MemoryType, Operator, Result, TableType, TagType,
-    TypeRef, ValType, WasmFeatures, WasmModuleResources,
+    TypeRef, ValType, VisitOperator, WasmFeatures, WasmModuleResources,
 };
 use indexmap::IndexMap;
 use std::{collections::HashSet, sync::Arc};
@@ -324,18 +324,16 @@ impl ModuleState {
                 }
             }
 
+            let resources = OperatorValidatorResources {
+                module: &self.module,
+                types,
+            };
             validator
-                .process_operator(
-                    &op,
-                    &OperatorValidatorResources {
-                        module: &self.module,
-                        types,
-                    },
-                )
-                .map_err(|e| e.set_offset(offset))?;
+                .with_resources(&resources)
+                .visit_operator(offset, &op)?;
         }
 
-        validator.finish().map_err(|e| e.set_offset(offset))?;
+        validator.finish(offset)?;
 
         // See comment in `RefFunc` above for why this is an assert.
         assert!(!uninserted_funcref);

commit 2af0eaff2d701f39181eb77475520e1679e1fe55

Fix an accidental regression from #697
This commit fixes a regression introduced in #697 which could cause a
panic when validating an invalid wasm module. The issue introduced was
that a [check that the control stack is non-empty][check] was lost in
the refactoring of the operator validator. This check ran for every
single operator and verified that there was a frame on the control stack
that the operator could be attached to, otherwise it means instructions
were present after the end of the function.

The current design of `VisitOperator` doesn't have an easy place to slot
this in so I decided to fix this via a different route than was
implemented before. Anything which operates on the control stack now
checks to see if it's empty instead of asserting it's non-empty.
Operators which don't touch the control stack are then checked by
ensuring that the `end` opcode which emptied the control stack was the
last operator processed in the function.

This exposed a minor issue where when validating const expressions the
offset that was passed in as the final offset of the expression was
actually the first offset of the expression.

Additionally this adds some tests to exercise this corner case (unsure
why the spec test suite doesn't have them already!)

[check]: https://github.com/bytecodealliance/wasm-tools/blob/8732e0bc8a579cd9f15d9134af997c5d3d95af5d/crates/wasmparser/src/validator/operators.rs#L581-L583

crates/wasmparser/src/validator/core.rs
@@ -136,13 +136,7 @@ impl ModuleState {
     ) -> Result<()> {
         self.module
             .check_global_type(&global.ty, features, offset)?;
-        self.check_const_expr(
-            &global.init_expr,
-            global.ty.content_type,
-            features,
-            types,
-            offset,
-        )?;
+        self.check_const_expr(&global.init_expr, global.ty.content_type, features, types)?;
         self.module.assert_mut().globals.push(global.ty);
         Ok(())
     }
@@ -161,7 +155,7 @@ impl ModuleState {
                 offset_expr,
             } => {
                 let ty = self.module.memory_at(memory_index, offset)?.index_type();
-                self.check_const_expr(&offset_expr, ty, features, types, offset)
+                self.check_const_expr(&offset_expr, ty, features, types)
             }
         }
     }
@@ -197,7 +191,7 @@ impl ModuleState {
                     ));
                 }
 
-                self.check_const_expr(&offset_expr, ValType::I32, features, types, offset)?;
+                self.check_const_expr(&offset_expr, ValType::I32, features, types)?;
             }
             ElementKind::Passive | ElementKind::Declared => {
                 if !features.bulk_memory {
@@ -219,7 +213,7 @@ impl ModuleState {
             let offset = items.original_position();
             match items.read()? {
                 ElementItem::Expr(expr) => {
-                    self.check_const_expr(&expr, e.ty, features, types, offset)?;
+                    self.check_const_expr(&expr, e.ty, features, types)?;
                 }
                 ElementItem::Func(f) => {
                     if e.ty != ValType::FuncRef {
@@ -244,7 +238,6 @@ impl ModuleState {
         expected_ty: ValType,
         features: &WasmFeatures,
         types: &TypeList,
-        offset: usize,
     ) -> Result<()> {
         let mut ops = expr.get_operators_reader();
         let mut validator = OperatorValidator::new_const_expr(features, expected_ty);
@@ -333,7 +326,7 @@ impl ModuleState {
                 .visit_operator(offset, &op)?;
         }
 
-        validator.finish(offset)?;
+        validator.finish(ops.original_position())?;
 
         // See comment in `RefFunc` above for why this is an assert.
         assert!(!uninserted_funcref);

crates/wasmparser/src/validator/operators.rs
@@ -69,6 +69,10 @@ pub(crate) struct OperatorValidator {
     control: Vec<Frame>,
     /// The `operands` is the current type stack.
     operands: Vec<Option<ValType>>,
+
+    /// Offset of the `end` instruction which emptied the `control` stack, which
+    /// must be the end of the function.
+    end_which_emptied_control: Option<usize>,
 }
 
 // This structure corresponds to `ctrl_frame` as specified at in the validation
@@ -128,6 +132,7 @@ impl OperatorValidator {
                 height: 0,
                 unreachable: false,
             }],
+            end_which_emptied_control: None,
         };
         let locals = OperatorValidatorTemp {
             inner: &mut ret,
@@ -159,6 +164,7 @@ impl OperatorValidator {
                 height: 0,
                 unreachable: false,
             }],
+            end_which_emptied_control: None,
         }
     }
 
@@ -209,7 +215,14 @@ impl OperatorValidator {
                 "control frames remain at end of function: END opcode expected"
             );
         }
+        if offset != self.end_which_emptied_control.unwrap() + 1 {
+            return Err(self.err_beyond_end(offset));
+        }
         Ok(())
+    }
+
+    fn err_beyond_end(&self, offset: usize) -> BinaryReaderError {
+        format_op_err!(offset, "operators remaining after end of function")
     }
 }
 
@@ -265,7 +278,10 @@ impl<'resources, R: WasmModuleResources> OperatorValidatorTemp<'_, 'resources, R
     /// expected and a type was successfully popped, but its exact type is
     /// indeterminate because the current block is unreachable.
     fn pop_operand(&mut self, offset: usize, expected: Option<ValType>) -> Result<Option<ValType>> {
-        let control = self.control.last().unwrap();
+        let control = match self.control.last() {
+            Some(c) => c,
+            None => return Err(self.err_beyond_end(offset)),
+        };
         let actual = if self.operands.len() == control.height {
             if control.unreachable {
                 None
@@ -358,7 +374,10 @@ impl<'resources, R: WasmModuleResources> OperatorValidatorTemp<'_, 'resources, R
     fn pop_ctrl(&mut self, offset: usize) -> Result<Frame> {
         // Read the expected type and expected height of the operand stack the
         // end of the frame.
-        let frame = self.control.last().unwrap();
+        let frame = match self.control.last() {
+            Some(f) => f,
+            None => return Err(self.err_beyond_end(offset)),
+        };
         let ty = frame.block_type;
         let height = frame.height;
 
@@ -996,6 +1015,10 @@ where
         }
         for ty in self.results(offset, frame.block_type)? {
             self.push_operand(offset, ty)?;
+        }
+
+        if self.control.is_empty() && self.end_which_emptied_control.is_none() {
+            self.end_which_emptied_control = Some(offset);
         }
         Ok(())
     }

fuzz/fuzz_targets/validate.rs
@@ -4,6 +4,7 @@ use libfuzzer_sys::*;
 use wasmparser::{Validator, WasmFeatures};
 
 fuzz_target!(|data: &[u8]| {
+    drop(env_logger::try_init());
     let byte1 = match data.get(0) {
         Some(byte) => byte,
         None => return,
@@ -31,5 +32,10 @@ fuzz_target!(|data: &[u8]| {
         sign_extension: (byte2 & 0b1000_0000) != 0,
     });
 
-    drop(validator.validate_all(&data[2..]));
+    let wasm = &data[2..];
+    if log::log_enabled!(log::Level::Debug) {
+        log::debug!("writing input to `test.wasm`");
+        std::fs::write("test.wasm", wasm).unwrap();
+    }
+    drop(validator.validate_all(wasm));
 });

tests/local/invalid/func.wast
@@ -1,0 +1,62 @@
+(assert_invalid
+  (module binary
+    "\00asm" "\01\00\00\00" ;; magic header
+
+    "\01\04"    ;; type section
+    "\01"       ;; 1 count
+    "\60\00\00" ;; no params or results
+
+    "\03\02"    ;; func section
+    "\01"       ;; 1 count
+    "\00"       ;; type 0
+
+    "\0a\05"    ;; code section
+    "\01"       ;; 1 count
+    "\03"       ;; size of function
+    "\00"       ;; no locals
+    "\0b"       ;; end
+    "\01"       ;; nop
+  )
+  "operators remaining after end of function")
+
+(assert_invalid
+  (module binary
+    "\00asm" "\01\00\00\00" ;; magic header
+
+    "\01\04"    ;; type section
+    "\01"       ;; 1 count
+    "\60\00\00" ;; no params or results
+
+    "\03\02"    ;; func section
+    "\01"       ;; 1 count
+    "\00"       ;; type 0
+
+    "\0a\05"    ;; code section
+    "\01"       ;; 1 count
+    "\03"       ;; size of function
+    "\00"       ;; no locals
+    "\0b"       ;; end
+    "\9d"       ;; f64.trunc
+  )
+  "operators remaining after end of function")
+
+(assert_invalid
+  (module binary
+    "\00asm" "\01\00\00\00" ;; magic header
+
+    "\01\04"    ;; type section
+    "\01"       ;; 1 count
+    "\60\00\00" ;; no params or results
+
+    "\03\02"    ;; func section
+    "\01"       ;; 1 count
+    "\00"       ;; type 0
+
+    "\0a\05"    ;; code section
+    "\01"       ;; 1 count
+    "\03"       ;; size of function
+    "\00"       ;; no locals
+    "\0b"       ;; end
+    "\0b"       ;; end
+  )
+  "operators remaining after end of function")