4314773297bacd1c64f58906c1bd1e239214a8af
Pat Hickey <phickey@fastly.com>
2022-09-02 14:24:56 -0700

    wasm-smith: limit instance decl type depth limit against max_nesting_depth
    additionally, rename InstanceTypeDef to InstanceTypeDecl, which matches
    the name in component-model/design/mvp/Binary.md

--- crates/wasm-smith/src/component/encode.rs
+++ crates/wasm-smith/src/component/encode.rs
@@ -220,30 +220,30 @@ impl Type {
                 let mut enc_inst_ty = wasm_encoder::InstanceType::new();
                 for def in &inst_ty.defs {
                     match def {
-                        InstanceTypeDef::CoreType(ty) => {
+                        InstanceTypeDecl::CoreType(ty) => {
                             ty.encode(enc_inst_ty.core_type());
                         }
-                        InstanceTypeDef::Type(ty) => {
+                        InstanceTypeDecl::Type(ty) => {
                             ty.encode(enc_inst_ty.ty());
                         }
-                        InstanceTypeDef::Export { name, ty } => {
+                        InstanceTypeDecl::Export { name, ty } => {
                             enc_inst_ty.export(name, *ty);
                         }
-                        InstanceTypeDef::Alias(Alias::Outer {
+                        InstanceTypeDecl::Alias(Alias::Outer {
                             count,
                             i,
                             kind: OuterAliasKind::Type(_),
                         }) => {
                             enc_inst_ty.alias_outer_type(*count, *i);
                         }
-                        InstanceTypeDef::Alias(Alias::Outer {
+                        InstanceTypeDecl::Alias(Alias::Outer {
                             count,
                             i,
                             kind: OuterAliasKind::CoreType(_),
                         }) => {
                             enc_inst_ty.alias_outer_core_type(*count, *i);
                         }
-                        InstanceTypeDef::Alias(_) => unreachable!(),
+                        InstanceTypeDecl::Alias(_) => unreachable!(),
                     }
                 }
                 enc.instance(&enc_inst_ty);
