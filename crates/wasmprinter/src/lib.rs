bytecodealliance/wasm-tools

commit 5dd59d75cca564248535fd39576048cf21afc365

Refactor and simplify wasm opcode printing (#795)
While looking through `wasmprinter` for other issues I was struck with
inspiration to simplify the printing of operators with the
`for_each_operator!` macro to avoid the verbosity of the full trait
implementation. Along the way I made a few other adjustments as well:

* `*RoundingAverage*` instructions now use `Avgr` in the name to match
  the instruction name.
* `MemArg` now specifies its `max_align` in a field to uniformly handle
  this without needing per-instruction treatment.
* Alignment of atomic operators now properly validates that the
  alignment must be specified at the maximum.
* The text representation of a relaxed simd instruction was updated to
  have `*_sat_*` in the name to match the in-Rust name.

crates/wasmprinter/src/lib.rs
@@ -965,56 +965,6 @@ impl Printer {
         }
     }
 
-    fn mem_instr(
-        &mut self,
-        state: &State,
-        name: &str,
-        memarg: &MemArg,
-        default_align: u32,
-    ) -> Result<()> {
-        self.result.push_str(name);
-        if memarg.memory != 0 {
-            self.result.push(' ');
-            self.print_idx(&state.core.memory_names, memarg.memory)?;
-        }
-        if memarg.offset != 0 {
-            write!(self.result, " offset={}", memarg.offset)?;
-        }
-        if memarg.align >= 32 {
-            bail!("alignment in memarg too large");
-        }
-        let align = 1 << memarg.align;
-        if default_align != align {
-            write!(self.result, " align={}", align)?;
-        }
-        Ok(())
-    }
-
-    fn print_blockty(&mut self, state: &mut State, ty: &BlockType, cur_depth: u32) -> Result<()> {
-        if let Some(name) = state
-            .core
-            .label_names
-            .get(&(state.core.funcs, state.core.labels))
-        {
-            self.result.push(' ');
-            name.write(&mut self.result);
-        }
-        match ty {
-            BlockType::Empty => {}
-            BlockType::Type(t) => {
-                self.result.push_str(" (result ");
-                self.print_valtype(*t)?;
-                self.result.push(')');
-            }
-            BlockType::FuncType(idx) => {
-                self.print_core_functype_idx(state, *idx, false, None)?;
-            }
-        }
-        write!(self.result, "  ;; label = @{}", cur_depth)?;
-        state.core.labels += 1;
-        Ok(())
-    }
-
     fn print_exports(&mut self, state: &State, data: ExportSectionReader) -> Result<()> {
         for export in data.into_iter_with_offsets() {
             let (offset, export) = export?;

crates/wasmparser/src/lib.rs
@@ -533,7 +533,7 @@ macro_rules! for_each_operator {
             @simd I8x16MinU => visit_i8x16_min_u
             @simd I8x16MaxS => visit_i8x16_max_s
             @simd I8x16MaxU => visit_i8x16_max_u
-            @simd I8x16RoundingAverageU => visit_i8x16_avgr_u
+            @simd I8x16AvgrU => visit_i8x16_avgr_u
             @simd I16x8ExtAddPairwiseI8x16S => visit_i16x8_extadd_pairwise_i8x16_s
             @simd I16x8ExtAddPairwiseI8x16U => visit_i16x8_extadd_pairwise_i8x16_u
             @simd I16x8Abs => visit_i16x8_abs
@@ -561,7 +561,7 @@ macro_rules! for_each_operator {
             @simd I16x8MinU => visit_i16x8_min_u
             @simd I16x8MaxS => visit_i16x8_max_s
             @simd I16x8MaxU => visit_i16x8_max_u
-            @simd I16x8RoundingAverageU => visit_i16x8_avgr_u
+            @simd I16x8AvgrU => visit_i16x8_avgr_u
             @simd I16x8ExtMulLowI8x16S => visit_i16x8_extmul_low_i8x16_s
             @simd I16x8ExtMulHighI8x16S => visit_i16x8_extmul_high_i8x16_s
             @simd I16x8ExtMulLowI8x16U => visit_i16x8_extmul_low_i8x16_u

crates/wasm-encoder/src/core/code.rs
@@ -647,7 +647,7 @@ pub enum Instruction<'a> {
     I8x16MinU,
     I8x16MaxS,
     I8x16MaxU,
-    I8x16RoundingAverageU,
+    I8x16AvgrU,
     I16x8ExtAddPairwiseI8x16S,
     I16x8ExtAddPairwiseI8x16U,
     I16x8Abs,
@@ -675,7 +675,7 @@ pub enum Instruction<'a> {
     I16x8MinU,
     I16x8MaxS,
     I16x8MaxU,
-    I16x8RoundingAverageU,
+    I16x8AvgrU,
     I16x8ExtMulLowI8x16S,
     I16x8ExtMulHighI8x16S,
     I16x8ExtMulLowI8x16U,
@@ -1783,7 +1783,7 @@ impl Encode for Instruction<'_> {
                 sink.push(0xFD);
                 0x79u32.encode(sink);
             }
-            Instruction::I8x16RoundingAverageU => {
+            Instruction::I8x16AvgrU => {
                 sink.push(0xFD);
                 0x7Bu32.encode(sink);
             }
@@ -1903,7 +1903,7 @@ impl Encode for Instruction<'_> {
                 sink.push(0xFD);
                 0x99u32.encode(sink);
             }
-            Instruction::I16x8RoundingAverageU => {
+            Instruction::I16x8AvgrU => {
                 sink.push(0xFD);
                 0x9Bu32.encode(sink);
             }

crates/wasm-mutate/src/mutators/peephole/dfg.rs
@@ -945,7 +945,7 @@ impl<'a> DFGBuilder {
                 Operator::I8x16MinU => self.binop(idx, Lang::I8x16MinU),
                 Operator::I8x16MaxS => self.binop(idx, Lang::I8x16MaxS),
                 Operator::I8x16MaxU => self.binop(idx, Lang::I8x16MaxU),
-                Operator::I8x16RoundingAverageU => self.binop(idx, Lang::I8x16AvgrU),
+                Operator::I8x16AvgrU => self.binop(idx, Lang::I8x16AvgrU),
 
                 Operator::I16x8ExtAddPairwiseI8x16S => {
                     self.unop(idx, Lang::I16x8ExtAddPairwiseI8x16S)
@@ -978,7 +978,7 @@ impl<'a> DFGBuilder {
                 Operator::I16x8MinU => self.binop(idx, Lang::I16x8MinU),
                 Operator::I16x8MaxS => self.binop(idx, Lang::I16x8MaxS),
                 Operator::I16x8MaxU => self.binop(idx, Lang::I16x8MaxU),
-                Operator::I16x8RoundingAverageU => self.binop(idx, Lang::I16x8AvgrU),
+                Operator::I16x8AvgrU => self.binop(idx, Lang::I16x8AvgrU),
                 Operator::I16x8ExtMulLowI8x16S => self.binop(idx, Lang::I16x8ExtMulLowI8x16S),
                 Operator::I16x8ExtMulHighI8x16S => self.binop(idx, Lang::I16x8ExtMulHighI8x16S),
                 Operator::I16x8ExtMulLowI8x16U => self.binop(idx, Lang::I16x8ExtMulLowI8x16U),

crates/wasm-mutate/src/mutators/peephole/eggsy/encoder/expr2wasm.rs
@@ -601,7 +601,7 @@ pub fn expr2wasm(
                     Lang::I8x16MinU(_) => insn(Instruction::I8x16MinU),
                     Lang::I8x16MaxS(_) => insn(Instruction::I8x16MaxS),
                     Lang::I8x16MaxU(_) => insn(Instruction::I8x16MaxU),
-                    Lang::I8x16AvgrU(_) => insn(Instruction::I8x16RoundingAverageU),
+                    Lang::I8x16AvgrU(_) => insn(Instruction::I8x16AvgrU),
 
                     Lang::I16x8ExtAddPairwiseI8x16S(_) => {
                         insn(Instruction::I16x8ExtAddPairwiseI8x16S)
@@ -634,7 +634,7 @@ pub fn expr2wasm(
                     Lang::I16x8MinU(_) => insn(Instruction::I16x8MinU),
                     Lang::I16x8MaxS(_) => insn(Instruction::I16x8MaxS),
                     Lang::I16x8MaxU(_) => insn(Instruction::I16x8MaxU),
-                    Lang::I16x8AvgrU(_) => insn(Instruction::I16x8RoundingAverageU),
+                    Lang::I16x8AvgrU(_) => insn(Instruction::I16x8AvgrU),
                     Lang::I16x8ExtMulLowI8x16S(_) => insn(Instruction::I16x8ExtMulLowI8x16S),
                     Lang::I16x8ExtMulHighI8x16S(_) => insn(Instruction::I16x8ExtMulHighI8x16S),
                     Lang::I16x8ExtMulLowI8x16U(_) => insn(Instruction::I16x8ExtMulLowI8x16U),

crates/wasm-smith/src/core/code_builder.rs
@@ -424,7 +424,7 @@ instructions! {
     (Some(simd_v128_v128_on_stack), i8x16_min_u, Vector),
     (Some(simd_v128_v128_on_stack), i8x16_max_s, Vector),
     (Some(simd_v128_v128_on_stack), i8x16_max_u, Vector),
-    (Some(simd_v128_v128_on_stack), i8x16_rounding_average_u, Vector),
+    (Some(simd_v128_v128_on_stack), i8x16_avgr_u, Vector),
     (Some(simd_v128_on_stack), i16x8_extadd_pairwise_i8x16s, Vector),
     (Some(simd_v128_on_stack), i16x8_extadd_pairwise_i8x16u, Vector),
     (Some(simd_v128_on_stack), i16x8_abs, Vector),
@@ -452,7 +452,7 @@ instructions! {
     (Some(simd_v128_v128_on_stack), i16x8_min_u, Vector),
     (Some(simd_v128_v128_on_stack), i16x8_max_s, Vector),
     (Some(simd_v128_v128_on_stack), i16x8_max_u, Vector),
-    (Some(simd_v128_v128_on_stack), i16x8_rounding_average_u, Vector),
+    (Some(simd_v128_v128_on_stack), i16x8_avgr_u, Vector),
     (Some(simd_v128_v128_on_stack), i16x8_extmul_low_i8x16s, Vector),
     (Some(simd_v128_v128_on_stack), i16x8_extmul_high_i8x16s, Vector),
     (Some(simd_v128_v128_on_stack), i16x8_extmul_low_i8x16u, Vector),
@@ -3834,7 +3834,7 @@ simd_binop!(I8x16MinS, i8x16_min_s);
 simd_binop!(I8x16MinU, i8x16_min_u);
 simd_binop!(I8x16MaxS, i8x16_max_s);
 simd_binop!(I8x16MaxU, i8x16_max_u);
-simd_binop!(I8x16RoundingAverageU, i8x16_rounding_average_u);
+simd_binop!(I8x16AvgrU, i8x16_avgr_u);
 simd_unop!(I16x8ExtAddPairwiseI8x16S, i16x8_extadd_pairwise_i8x16s);
 simd_unop!(I16x8ExtAddPairwiseI8x16U, i16x8_extadd_pairwise_i8x16u);
 simd_unop!(I16x8Abs, i16x8_abs);
@@ -3862,7 +3862,7 @@ simd_binop!(I16x8MinS, i16x8_min_s);
 simd_binop!(I16x8MinU, i16x8_min_u);
 simd_binop!(I16x8MaxS, i16x8_max_s);
 simd_binop!(I16x8MaxU, i16x8_max_u);
-simd_binop!(I16x8RoundingAverageU, i16x8_rounding_average_u);
+simd_binop!(I16x8AvgrU, i16x8_avgr_u);
 simd_binop!(I16x8ExtMulLowI8x16S, i16x8_extmul_low_i8x16s);
 simd_binop!(I16x8ExtMulHighI8x16S, i16x8_extmul_high_i8x16s);
 simd_binop!(I16x8ExtMulLowI8x16U, i16x8_extmul_low_i8x16u);

commit e5e916b1d93232d3a0d3007ce12b712b7e2af5f8

Update blockty printing and space handling
This commit fixes a minor issue found during fuzzing that is a
regression from #795. Specifically some encodings of block types would
lead to printing the binary format with two spaces after the `block`
instruction but printing again would only print one space. This was due
to a number of issues fixed in this commit:

* Primarily `wasmprinter` has been reorganized to no longer assume some
  methods print the leading whitespace. This basically "randomly" moves
  around space printing in `wasmprinter`. To help make this less
  "random" I've also added a test in `roundtrip.rs` that all whitespace
  tokens in the `wasmprinter`-produced output are not "bad whitespace"
  which means there's no trailing whitespace or double-spaces printed.
  This caught a few issues with prior iterations of this commit

* Additionally when looking more at block type parsing and printing I
  was reminded that much of the processing here was an artifact of
  historical compatibility with `wabt` where `wabt` canonicalized
  non-MVP encodings into MVP encodings where possible. I removed this
  canonicalization to remain faithful to the original binary format
  since `wabt` is no longer used as a reference test. This was
  orthogonal to the spacing changes above but was something that seemed
  like it should be fixed regardless. A `*.dump` test was added to
  assert that the encodings are as expected and not canonicalized, even
  when possible.

crates/wasmprinter/src/lib.rs
@@ -527,9 +527,9 @@ impl Printer {
             &states.last().unwrap().core.type_names,
             states.last().unwrap().core.types.len() as u32,
         )?;
-        self.result.push(' ');
         let ty = match ty {
             wasmparser::CoreType::Func(ty) => {
+                self.result.push(' ');
                 self.start_group("func");
                 self.print_func_type(states.last().unwrap(), &ty, None)?;
                 self.end_group();
@@ -591,21 +591,13 @@ impl Printer {
         &mut self,
         state: &State,
         idx: u32,
-        always_print_type: bool,
         names_for: Option<u32>,
     ) -> Result<Option<u32>> {
-        if always_print_type {
-            self.print_type_ref(state, idx, true, None)?;
-        }
+        self.print_type_ref(state, idx, true, None)?;
 
         match state.core.types.get(idx as usize) {
             Some(Some(ty)) => self.print_func_type(state, ty, names_for).map(Some),
-            Some(None) | None => {
-                if !always_print_type {
-                    self.print_type_ref(state, idx, true, None)?;
-                }
-                Ok(None)
-            }
+            Some(None) | None => Ok(None),
         }
     }
 
@@ -695,10 +687,10 @@ impl Printer {
     fn print_import_ty(&mut self, state: &State, ty: &TypeRef, index: bool) -> Result<()> {
         match ty {
             TypeRef::Func(f) => {
-                self.start_group("func");
+                self.start_group("func ");
                 if index {
-                    self.result.push(' ');
                     self.print_name(&state.core.func_names, state.core.funcs)?;
+                    self.result.push(' ');
                 }
                 self.print_type_ref(state, *f, true, None)?;
             }
@@ -742,9 +734,9 @@ impl Printer {
     fn print_tag_type(&mut self, state: &State, ty: &TagType, index: bool) -> Result<()> {
         self.start_group("tag ");
         if index {
-            write!(self.result, "(;{};)", state.core.tags)?;
+            write!(self.result, "(;{};) ", state.core.tags)?;
         }
-        self.print_core_functype_idx(state, ty.func_type_idx, true, None)?;
+        self.print_core_functype_idx(state, ty.func_type_idx, None)?;
         Ok(())
     }
 
@@ -838,8 +830,9 @@ impl Printer {
             self.start_group("func ");
             let func_idx = state.core.funcs;
             self.print_name(&state.core.func_names, func_idx)?;
+            self.result.push(' ');
             let params = self
-                .print_core_functype_idx(state, ty, true, Some(func_idx))?
+                .print_core_functype_idx(state, ty, Some(func_idx))?
                 .unwrap_or(0);
 
             let mut first = true;
@@ -1015,7 +1008,7 @@ impl Printer {
         core: bool,
         bounds: Option<TypeBounds>,
     ) -> Result<()> {
-        self.result.push_str(" (type ");
+        self.result.push_str("(type ");
         let closing = match bounds {
             Some(TypeBounds::Eq) => {
                 self.result.push_str("(eq ");
@@ -1703,19 +1696,19 @@ impl Printer {
     ) -> Result<()> {
         match ty {
             ComponentTypeRef::Module(idx) => {
-                self.start_group("core module");
+                self.start_group("core module ");
                 if index {
-                    self.result.push(' ');
                     self.print_name(&state.core.module_names, state.core.modules as u32)?;
+                    self.result.push(' ');
                 }
                 self.print_type_ref(state, *idx, true, None)?;
                 self.end_group();
             }
             ComponentTypeRef::Func(idx) => {
-                self.start_group("func");
+                self.start_group("func ");
                 if index {
-                    self.result.push(' ');
                     self.print_name(&state.component.func_names, state.component.funcs)?;
+                    self.result.push(' ');
                 }
                 self.print_type_ref(state, *idx, false, None)?;
                 self.end_group();
@@ -1738,19 +1731,19 @@ impl Printer {
                 self.print_type_ref(state, *idx, false, Some(*bounds))?;
             }
             ComponentTypeRef::Instance(idx) => {
-                self.start_group("instance");
+                self.start_group("instance ");
                 if index {
-                    self.result.push(' ');
                     self.print_name(&state.component.instance_names, state.component.instances)?;
+                    self.result.push(' ');
                 }
                 self.print_type_ref(state, *idx, false, None)?;
                 self.end_group();
             }
             ComponentTypeRef::Component(idx) => {
-                self.start_group("component");
+                self.start_group("component ");
                 if index {
-                    self.result.push(' ');
                     self.print_name(&state.component.component_names, state.component.components)?;
+                    self.result.push(' ');
                 }
                 self.print_type_ref(state, *idx, false, None)?;
                 self.end_group();

crates/wasmprinter/src/operator.rs
@@ -33,26 +33,27 @@ impl<'a, 'b> PrintOperator<'a, 'b> {
             .label_names
             .get(&(self.state.core.funcs, self.state.core.labels))
         {
-            self.printer.result.push(' ');
             name.write(&mut self.printer.result);
+            self.printer.result.push(' ');
         }
         match ty {
             BlockType::Empty => {}
             BlockType::Type(t) => {
                 self.push_str("(result ");
                 self.printer.print_valtype(t)?;
-                self.push_str(")");
+                self.push_str(") ");
             }
             BlockType::FuncType(idx) => {
                 self.printer
-                    .print_core_functype_idx(self.state, idx, false, None)?;
+                    .print_core_functype_idx(self.state, idx, None)?;
+                self.printer.result.push(' ');
             }
         }
         // Note that 1 is added to the current depth here since if a block type
         // is being printed then a block is being created which will increase
         // the label depth of the block itself.
         let depth = self.cur_depth();
-        write!(self.result(), "  ;; label = @{}", depth + 1)?;
+        write!(self.result(), ";; label = @{}", depth + 1)?;
         self.state.core.labels += 1;
         Ok(())
     }
@@ -142,6 +143,12 @@ impl<'a, 'b> PrintOperator<'a, 'b> {
     }
 
     fn memarg(&mut self, memarg: MemArg) -> Result<()> {
+        // Remove the leading ' ' inserted by the macro below since memarg may
+        // not actually print anything if all of its parameters are defaulted.
+        // Ideally we wouldn't rely on the ability to pop here but this ends up
+        // being the easiest.
+        assert_eq!(self.printer.result.pop(), Some(' '));
+
         if memarg.memory != 0 {
             self.result().push(' ');
             self.memory_index(memarg.memory)?;

crates/wast/src/core/resolve/names.rs
@@ -384,54 +384,21 @@ impl<'a, 'b> ExprResolver<'a, 'b> {
     }
 
     fn resolve_block_type(&mut self, bt: &mut BlockType<'a>) -> Result<(), Error> {
-        // Ok things get interesting here. First off when parsing `bt`
-        // *optionally* has an index and a function type listed. If
-        // they're both not present it's equivalent to 0 params and 0
-        // results.
+        // If the index is specified on this block type then that's the source
+        // of resolution and the resolver step here will verify the inline type
+        // matches. Note that indexes may come from the source text itself but
+        // may also come from being injected as part of the type expansion phase
+        // of resolution.
         //
-        // In MVP wasm blocks can have 0 params and 0-1 results. Now
-        // there's also multi-value. We want to prefer MVP wasm wherever
-        // possible (for backcompat) so we want to list this block as
-        // being an "MVP" block if we can. The encoder only has
-        // `BlockType` to work with, so it'll be looking at `params` and
-        // `results` to figure out what to encode. If `params` and
-        // `results` fit within MVP, then it uses MVP encoding
-        //
-        // To put all that together, here we handle:
-        //
-        // * If the `index` was specified, resolve it and use it as the
-        //   source of truth. If this turns out to be an MVP type,
-        //   record it as such.
-        // * Otherwise use `params` and `results` as the source of
-        //   truth. *If* this were a non-MVP compatible block `index`
-        //   would be filled by by `tyexpand.rs`.
-        //
-        // tl;dr; we handle the `index` here if it's set and then fill
-        // out `params` and `results` if we can, otherwise no work
-        // happens.
+        // If no type is present then that means that the inline type is not
+        // present or has 0-1 results. In that case the nested value types are
+        // resolved, if they're there, to get encoded later on.
         if bt.ty.index.is_some() {
-            let (ty, _) = self.resolver.resolve_type_use(&mut bt.ty)?;
-            let n = match ty {
-                Index::Num(n, _) => *n,
-                Index::Id(_) => panic!("expected `Num`"),
-            };
-            let ty = match self.resolver.type_info.get(n as usize) {
-                Some(TypeInfo::Func { params, results }) => (params, results),
-                _ => return Ok(()),
-            };
-            if ty.0.len() == 0 && ty.1.len() <= 1 {
-                let mut inline = FunctionType::default();
-                inline.results = ty.1.clone();
-                bt.ty.inline = Some(inline);
-                bt.ty.index = None;
-            }
-        }
-
-        // If the inline annotation persists to this point then resolve
-        // all of its inline value types.
-        if let Some(inline) = &mut bt.ty.inline {
+            self.resolver.resolve_type_use(&mut bt.ty)?;
+        } else if let Some(inline) = &mut bt.ty.inline {
             inline.resolve(self.resolver)?;
         }
+
         Ok(())
     }
 

tests/dump/blockty.wat
@@ -1,0 +1,14 @@
+(module
+  (type $empty (func))
+  (type $t (func (result i32)))
+  (func
+    (block)
+    (block (result i32))
+    (block (param i32))
+    (block (param i32) (result i32))
+    (block (param i32) (result i32 i32))
+    (block (type $t))
+    (block (type $t) (result i32))
+    (block (type $empty))
+  )
+)

tests/dump/blockty.wat.dump
@@ -1,0 +1,44 @@
+  0x0 | 00 61 73 6d | version 1 (Module)
+      | 01 00 00 00
+  0x8 | 01 17       | type section
+  0xa | 05          | 5 count
+  0xb | 60 00 00    | [type 0] Func(FuncType { params: [], returns: [] })
+  0xe | 60 00 01 7f | [type 1] Func(FuncType { params: [], returns: [I32] })
+ 0x12 | 60 01 7f 00 | [type 2] Func(FuncType { params: [I32], returns: [] })
+ 0x16 | 60 01 7f 01 | [type 3] Func(FuncType { params: [I32], returns: [I32] })
+      | 7f         
+ 0x1b | 60 01 7f 02 | [type 4] Func(FuncType { params: [I32], returns: [I32, I32] })
+      | 7f 7f      
+ 0x21 | 03 02       | func section
+ 0x23 | 01          | 1 count
+ 0x24 | 00          | [func 0] type 0
+ 0x25 | 0a 1c       | code section
+ 0x27 | 01          | 1 count
+============== func 0 ====================
+ 0x28 | 1a          | size of function
+ 0x29 | 00          | 0 local blocks
+ 0x2a | 02 40       | block blockty:Empty
+ 0x2c | 0b          | end
+ 0x2d | 02 7f       | block blockty:Type(I32)
+ 0x2f | 0b          | end
+ 0x30 | 02 02       | block blockty:FuncType(2)
+ 0x32 | 0b          | end
+ 0x33 | 02 03       | block blockty:FuncType(3)
+ 0x35 | 0b          | end
+ 0x36 | 02 04       | block blockty:FuncType(4)
+ 0x38 | 0b          | end
+ 0x39 | 02 01       | block blockty:FuncType(1)
+ 0x3b | 0b          | end
+ 0x3c | 02 01       | block blockty:FuncType(1)
+ 0x3e | 0b          | end
+ 0x3f | 02 00       | block blockty:FuncType(0)
+ 0x41 | 0b          | end
+ 0x42 | 0b          | end
+ 0x43 | 00 12       | custom section
+ 0x45 | 04 6e 61 6d | name: "name"
+      | 65         
+ 0x4a | 04 0b       | type names
+ 0x4c | 02          | 2 count
+ 0x4d | 00 05 65 6d | Naming { index: 0, name: "empty" }
+      | 70 74 79   
+ 0x54 | 01 01 74    | Naming { index: 1, name: "t" }

tests/roundtrip.rs
@@ -206,6 +206,32 @@ impl TestState {
                 .context("failed to compare original `wat` with roundtrip `wat`")?;
         }
 
+        // Test that the `wasmprinter`-printed bytes have "pretty" whitespace
+        // which means that all whitespace is either categorized as leading
+        // whitespace or a single space. Examples of "bad whitespace" are:
+        //
+        // * trailing whitespace at the end of a line
+        // * two spaces in a row
+        //
+        // Both of these cases indicate possible bugs in `wasmprinter` itself
+        // which while they don't actually affect the meaning they do "affect"
+        // humans reading the output.
+        for token in wast::lexer::Lexer::new(&string).allow_confusing_unicode(true) {
+            let ws = match token? {
+                wast::lexer::Token::Whitespace(ws) => ws,
+                _ => continue,
+            };
+            if ws.starts_with("\n") || ws == " " {
+                continue;
+            }
+            let offset = ws.as_ptr() as usize - string.as_ptr() as usize;
+            let span = wast::token::Span::from_offset(offset);
+            let msg = format!("found non-one-length whitespace in `wasmprinter` output: {ws:?}");
+            let mut err = wast::Error::new(span, msg);
+            err.set_text(&string);
+            return Err(err.into());
+        }
+
         Ok(())
     }
 