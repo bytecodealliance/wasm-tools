e3bb2da0533c09748015824fe049807d09a44189
Alex Crichton <alex@alexcrichton.com>
2022-10-25 11:16:47 -0700

    Fix the `publish.rs` script and version bounds
    Makes it so the `./publish` script can now be run after this commit.

--- publish.rs
+++ publish.rs
@@ -242,7 +242,7 @@ fn bump_version(krate: &mut Crate, crates: &mut [Crate], bumps: &[(String, bool)
             if !is_deps || !line.starts_with(&format!("{} ", other.name)) {
                 continue;
             }
-            if !line.contains(&other.version) {
+            if !line.contains(&other.version) && !line.contains("workspace = true") {
                 if !line.contains("version =") || !krate.publish {
                     continue;
                 }
