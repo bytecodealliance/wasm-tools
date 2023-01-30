6bb0e799cb4cee265f42a213b2aabd657a2f8446
Pat Hickey <phickey@fastly.com>
2023-01-30 11:58:09 -0800

    corrections

--- crates/wasm-encoder/src/core/producers.rs
+++ crates/wasm-encoder/src/core/producers.rs
@@ -1,4 +1,4 @@
-use crate::{encode_section, CustomSection, Encode, Section, SectionId};
+use crate::{CustomSection, Encode, Section, SectionId};
 
 /// An encoder for the [producers custom
 /// section](https://github.com/WebAssembly/tool-conventions/blob/main/ProducersSection.md).
@@ -8,14 +8,18 @@ use crate::{encode_section, CustomSection, Encode, Section, SectionId};
 /// # Example
 ///
 /// ```
-/// use wasm_encoder::{ProducersSection, Module, SymbolTable};
+/// use wasm_encoder::{ProducersSection, ProducersField, Module};
 ///
 /// // Create a new producers section.
+/// let mut field = ProducersField::new();
+/// field.value("clang", "14.0.4");
+/// field.value("rustc", "1.66.1 (90743e729 2023-01-10)");
 /// let mut producers = ProducersSection::new();
+/// producers.field("processed-by", &field);
 ///
-/// // Add the linking section to a new Wasm module and get the encoded bytes.
+/// // Add the producers section to a new Wasm module and get the encoded bytes.
 /// let mut module = Module::new();
-/// module.section(&linking);
+/// module.section(&producers);
 /// let wasm_bytes = module.finish();
 /// ```
 #[derive(Clone, Debug)]
@@ -25,14 +29,17 @@ pub struct ProducersSection {
 }
 
 impl ProducersSection {
-    /// Construct a new encoder for the linking custom section.
+    /// Construct an empty encoder for the producers custom section.
     pub fn new() -> Self {
         Self::default()
     }
 
-    /// TK
-    pub fn field(&mut self, producers_field: &ProducersField) -> &mut Self {
-        producers_field.encode(&mut self.bytes);
+    /// Add a field to the section. The spec recommends names for this section
+    /// are "language", "processed-by", and "sdk".  Each field in section must
+    /// have a unique name.
+    pub fn field(&mut self, name: &str, values: &ProducersField) -> &mut Self {
+        name.encode(&mut self.bytes);
+        values.encode(&mut self.bytes);
         self.num_fields += 1;
         self
     }
@@ -50,7 +57,8 @@ impl Default for ProducersSection {
 impl Encode for ProducersSection {
     fn encode(&self, sink: &mut Vec<u8>) {
         let mut data = Vec::new();
-        encode_section(&mut data, self.num_fields, &self.bytes);
+        self.num_fields.encode(&mut data);
+        data.extend(&self.bytes);
 
         CustomSection {
             name: "producers",
@@ -66,7 +74,7 @@ impl Section for ProducersSection {
     }
 }
 
-/// TK
+/// The value of a field in the producers custom section
 #[derive(Clone, Debug)]
 pub struct ProducersField {
     bytes: Vec<u8>,
@@ -74,52 +82,97 @@ pub struct ProducersField {
 }
 
 impl ProducersField {
-    /// TK
-    pub fn new(name: &str) -> Self {
-        let mut bytes = Vec::new();
-        name.encode(&mut bytes);
-        ProducersField {
-            bytes,
-            num_values: 0,
-        }
+    /// Construct an empty encoder for a producers field value
+    pub fn new() -> Self {
+        ProducersField::default()
     }
 
-    /// TK
-    pub fn value(&mut self, value: &ProducersFieldValue) -> &mut Self {
-        value.encode(&mut self.bytes);
+    /// Add a value to the field encoder. Each value in a field must have a
+    /// unique name. If there is no sensible value for `version`, use the
+    /// empty string.
+    pub fn value(&mut self, name: &str, version: &str) -> &mut Self {
+        name.encode(&mut self.bytes);
+        version.encode(&mut self.bytes);
         self.num_values += 1;
         self
     }
 }
 
+impl Default for ProducersField {
+    fn default() -> Self {
+        Self {
+            bytes: Vec::new(),
+            num_values: 0,
+        }
+    }
+}
+
 impl Encode for ProducersField {
     fn encode(&self, sink: &mut Vec<u8>) {
-        encode_section(sink, self.num_values, &self.bytes);
+        self.num_values.encode(sink);
+        sink.extend(&self.bytes);
     }
 }
 
-/// TK
-#[derive(Clone, Debug)]
-pub struct ProducersFieldValue {
-    bytes: Vec<u8>,
-}
+#[cfg(test)]
+mod test {
+    #[test]
+    fn roundtrip_example() {
+        use crate::{Module, ProducersField, ProducersSection};
+        use wasmparser::{Parser, Payload, ProducersSectionReader};
 
-impl ProducersFieldValue {
-    /// TK
-    pub fn name(name: &str) -> Self {
-        Self::version(name, "")
-    }
-    /// TK
-    pub fn version(name: &str, version: &str) -> Self {
-        let mut bytes = Vec::new();
-        name.encode(&mut bytes);
-        version.encode(&mut bytes);
-        ProducersFieldValue { bytes }
-    }
-}
+        // Create a new producers section.
+        let mut field = ProducersField::new();
+        field.value("clang", "14.0.4");
+        field.value("rustc", "1.66.1");
+        let mut producers = ProducersSection::new();
+        producers.field("processed-by", &field);
 
-impl Encode for ProducersFieldValue {
-    fn encode(&self, sink: &mut Vec<u8>) {
-        sink.extend_from_slice(&self.bytes)
+        // Add the producers section to a new Wasm module and get the encoded bytes.
+        let mut module = Module::new();
+        module.section(&producers);
+        let wasm_bytes = module.finish();
+
+        let mut parser = Parser::new(0).parse_all(&wasm_bytes);
+        let payload = parser
+            .next()
+            .expect("parser is not empty")
+            .expect("element is a payload");
+        match payload {
+            Payload::Version { .. } => {}
+            _ => panic!(""),
+        }
+        let payload = parser
+            .next()
+            .expect("parser is not empty")
+            .expect("element is a payload");
+        match payload {
+            Payload::CustomSection(c) => {
+                assert_eq!(c.name(), "producers");
+                let mut section = ProducersSectionReader::new(c.data(), c.data_offset())
+                    .expect("readable as a producers section")
+                    .into_iter();
+                let field = section
+                    .next()
+                    .expect("section has an element")
+                    .expect("element is a producers field");
+                assert_eq!(field.name, "processed-by");
+                let mut values = field.values.into_iter();
+                let value = values
+                    .next()
+                    .expect("values has an element")
+                    .expect("element is a producers field value");
+                assert_eq!(value.name, "clang");
+                assert_eq!(value.version, "14.0.4");
+
+                let value = values
+                    .next()
+                    .expect("values has another element")
+                    .expect("element is a producers field value");
+                assert_eq!(value.name, "rustc");
+                assert_eq!(value.version, "1.66.1");
+            }
+            _ => panic!("unexpected payload"),
+        }
     }
 }
