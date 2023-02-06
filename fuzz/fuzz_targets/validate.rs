e2159bbdbfdebc214b7c4d6b07fe7ec537812f02
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2023-02-06 10:55:55 +0100

    Attempt to fix fuzz/fuzz_targets/validate.rs

--- fuzz/fuzz_targets/validate.rs
+++ fuzz/fuzz_targets/validate.rs
@@ -37,8 +37,9 @@ fuzz_target!(|data: &[u8]| {
         saturating_float_to_int: (byte2 & 0b0100_0000) != 0,
         sign_extension: (byte2 & 0b1000_0000) != 0,
         memory_control: (byte3 & 0b0000_0001) != 0,
+        function_references: (byte3 & 0b0000_0010) != 0,
     });
-    let use_maybe_invalid = byte3 & 0b0000_0010 != 0;
+    let use_maybe_invalid = byte3 & 0b0000_0100 != 0;
 
     let wasm = &data[3..];
     if log::log_enabled!(log::Level::Debug) {
