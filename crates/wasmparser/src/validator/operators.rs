bytecodealliance/wasm-tools

commit e5293d587f463e67d42ca26b151cd7afdc0a5e17

Function references (#701)
* [WIP] Parse reference types from binary

This identifies reference types' magic numbers and advances the parser.
It parses heap types and assembles them into a type for non-nullable
references. But, it does not actually change the syntax yet so it just
panics. It remains to be implemented for nullable references and the
sugars.

* Add nullable reference types and desugar forms

Untested

* [WIP] Factor RefType out of ValType

This checks for wasmparser, but no tests and no other crates yet

* [func-refs] Fix parser tests, finish ref type parsing

* [func-refs] Support ref types in pretty-printer

* [func-refs] Fix pretty-printer bugs

Missing parenthesis and printing a valtype instead of a heap type for
func.ref

* [func-refs] Add support for validator func.ref

* [func-refs] Add syntax in encoder but not support

* [func-refs] Fix printer giving bad types of indices

* Clean up debugging nonsense

* [func-refs] Implement subtyping

* Implement a binary reader for function references instructions. (#1)

* Change ref.null to take heaptype, not valtype

* Update error message to match tests

* Hack around error message test

* [WIP] [func-refs] Add call_ref static semantics

WIP because untested

* Static semantics for function references (#2)

Further changes to validation need to be made, at least including adding subtyping to more places.

* [func-refs] Add subtyping to elements

* Subtyping relation (#4)

* Refactor the validator for function references (#5)

Co-authored-by: Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
Co-authored-by: cosine <CosineP@users.noreply.github.com>

* [func-refs] Assume matches V128 V128 = true

It looks to me that the function references spec was forked from
webassembly before vectors were merged, so it has no definitive answer
on V128 subtyping.  It also means we accidentally reverted `select`

fuzz/fuzz_targets/validate.rs
@@ -37,8 +37,9 @@ fuzz_target!(|data: &[u8]| {
         saturating_float_to_int: (byte2 & 0b0100_0000) != 0,
         sign_extension: (byte2 & 0b1000_0000) != 0,
         memory_control: (byte3 & 0b0000_0001) != 0,
+        function_references: (byte3 & 0b0000_0010) != 0,
     });
-    let use_maybe_invalid = byte3 & 0b0000_0010 != 0;
+    let use_maybe_invalid = byte3 & 0b0000_0100 != 0;
 
     let wasm = &data[3..];
     if log::log_enabled!(log::Level::Debug) {

src/bin/wasm-tools/validate.rs
@@ -89,6 +89,7 @@ fn parse_features(arg: &str) -> Result<WasmFeatures> {
 
     const FEATURES: &[(&str, fn(&mut WasmFeatures) -> &mut bool)] = &[
         ("reference-types", |f| &mut f.reference_types),
+        ("function-references", |f| &mut f.function_references),
         ("simd", |f| &mut f.simd),
         ("threads", |f| &mut f.threads),
         ("bulk-memory", |f| &mut f.bulk_memory),

commit a86e29e845b2b390c7b945c03f84898dfec3c303

Fix validation panic with unreachable code
This fixes a fuzz-found regression from #701 where unreachable code
could end up triggering a panic during validation.

crates/wasmparser/src/validator/operators.rs
@@ -153,7 +153,7 @@ pub struct OperatorValidatorAllocations {
 ///
 /// This is used to manage the operand stack and notably isn't just `ValType` to
 /// handle unreachable code and the "bottom" type.
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 enum MaybeType {
     Bot,
     HeapBot,
@@ -431,17 +431,18 @@ impl<'resources, R: WasmModuleResources> OperatorValidatorTemp<'_, 'resources, R
         // matched against the state of the world to see if we could actually
         // pop it. If we shouldn't have popped it then it's passed to the slow
         // path to get pushed back onto the stack.
-        let popped = if let Some(MaybeType::Type(actual_ty)) = self.operands.pop() {
-            if Some(actual_ty) == expected {
-                if let Some(control) = self.control.last() {
-                    if self.operands.len() >= control.height {
-                        return Ok(MaybeType::Type(actual_ty));
+        let popped = match self.operands.pop() {
+            Some(MaybeType::Type(actual_ty)) => {
+                if Some(actual_ty) == expected {
+                    if let Some(control) = self.control.last() {
+                        if self.operands.len() >= control.height {
+                            return Ok(MaybeType::Type(actual_ty));
+                        }
                     }
                 }
+                Some(MaybeType::Type(actual_ty))
             }
-            Some(MaybeType::Type(actual_ty))
-        } else {
-            None
+            other => other,
         };
 
         self._pop_operand(expected, popped)

fuzz/fuzz_targets/validate.rs
@@ -42,16 +42,15 @@ fuzz_target!(|data: &[u8]| {
     let use_maybe_invalid = byte3 & 0b0000_0100 != 0;
 
     let wasm = &data[3..];
-    if log::log_enabled!(log::Level::Debug) {
-        log::debug!("writing input to `test.wasm`");
-        std::fs::write("test.wasm", wasm).unwrap();
-    }
     if use_maybe_invalid {
         let mut u = Unstructured::new(wasm);
         if let Ok(module) = MaybeInvalidModule::arbitrary(&mut u) {
-            drop(validator.validate_all(&module.to_bytes()));
+            let wasm = module.to_bytes();
+            wasm_tools_fuzz::log_wasm(&wasm, "");
+            drop(validator.validate_all(&wasm));
         }
     } else {
+        wasm_tools_fuzz::log_wasm(wasm, "");
         drop(validator.validate_all(wasm));
     }
 });

tests/local/invalid-unreachable.wast
@@ -1,0 +1,11 @@
+(assert_invalid
+  (module
+    (func
+      unreachable
+      select
+      loop
+        i32x4.eq
+      end
+    )
+  )
+  "expected v128 but nothing on stack")