00d1a980faba40ed4f8786b0663642061de33dc6
Rainy Sinclair <844493+itsrainy@users.noreply.github.com>
2023-03-08 11:11:58 -0500

    - Add a ReorderCustomSectionMutator (#954)
    - Move AddCustomSectionMutator to custom.rs
    - Add CustomSectionMutator to lib.rs so it starts getting used
    
    Co-authored-by: Nick Fitzgerald <fitzgen@gmail.com>

--- crates/wasm-mutate/src/mutators/custom.rs
+++ crates/wasm-mutate/src/mutators/custom.rs
@@ -72,9 +72,96 @@ impl Mutator for CustomSectionMutator {
     }
 }
 
+#[derive(Clone, Copy)]
+pub struct AddCustomSectionMutator;
+
+const MAX_NEW_DATA_LEN: usize = 100;
+const MAX_NEW_NAME_LEN: usize = 20;
+
+impl Mutator for AddCustomSectionMutator {
+    fn can_mutate(&self, config: &crate::WasmMutate) -> bool {
+        !config.reduce
+    }
+
+    fn mutate<'a>(
+        &self,
+        config: &'a mut crate::WasmMutate,
+    ) -> crate::Result<Box<dyn Iterator<Item = crate::Result<wasm_encoder::Module>> + 'a>> {
+        let num_sections = config.info().raw_sections.len();
+        let new_custom_section_idx = config.rng().gen_range(0..=num_sections);
+        let mut name = vec![];
+        config.raw_mutate(&mut name, MAX_NEW_NAME_LEN)?;
+        let name = String::from_utf8_lossy(&name);
+        let mut data = vec![];
+        config.raw_mutate(&mut data, MAX_NEW_DATA_LEN)?;
+
+        Ok(Box::new(std::iter::once(Ok(config.info().insert_section(
+            new_custom_section_idx,
+            &wasm_encoder::CustomSection {
+                name: &name,
+                data: &data,
+            },
+        )))))
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct ReorderCustomSectionMutator;
+
+impl Mutator for ReorderCustomSectionMutator {
+    fn can_mutate(&self, config: &crate::WasmMutate) -> bool {
+        config.info().has_custom_section() && config.info().raw_sections.len() > 1
+    }
+
+    fn mutate<'a>(
+        &self,
+        config: &'a mut crate::WasmMutate,
+    ) -> crate::Result<Box<dyn Iterator<Item = crate::Result<wasm_encoder::Module>> + 'a>> {
+        let custom_section_indices: Vec<_> = config
+            .info()
+            .raw_sections
+            .iter()
+            .enumerate()
+            .filter(|(_i, s)| s.id == wasm_encoder::SectionId::Custom as u8)
+            .map(|(i, _s)| i)
+            .collect();
+        assert!(!custom_section_indices.is_empty());
+
+        let src_idx = *custom_section_indices.choose(config.rng()).unwrap();
+        let num_sections = config.info().raw_sections.len();
+        let mut dest_idx;
+        loop {
+            dest_idx = config.rng().gen_range(0..num_sections);
+            if dest_idx != src_idx {
+                break;
+            }
+            config.consume_fuel(1)?;
+        }
+
+        Ok(Box::new(std::iter::once(Ok(config
+            .info()
+            .move_section(src_idx, dest_idx)))))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_add_custom_section() {
+        crate::mutators::match_mutation(
+            r#"
+                (module)
+            "#,
+            AddCustomSectionMutator,
+            r#"
+                (module
+                    (@custom "a" "b")
+                )
+            "#,
+        );
+    }
 
     #[test]
     fn test_grow_custom_section() {
@@ -142,5 +229,24 @@ mod tests {
                 )
             "#,
         );
+    }
+
+    #[test]
+    fn test_reorder_custom_section() {
+        crate::mutators::match_mutation(
+            r#"
+            (module
+                (@custom "name" "data")
+                (@custom "name2" "data")
+            )
+            "#,
+            ReorderCustomSectionMutator,
+            r#"
+            (module
+                (@custom "name2" "data")
+                (@custom "name" "data")
+            )
+            "#,
+        )
     }
 }
