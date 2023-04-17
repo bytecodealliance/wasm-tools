bytecodealliance/wasm-tools

commit d550bd5491f9433bc42601f03a632ade53805b62

`wasmparser`: turn table index in the elements section into an `Option` (#957)
* `wasmparser`: turn table index in the elements section into an `Option`

* Update test

crates/wasm-mutate/src/mutators/translate.rs
@@ -263,7 +263,7 @@ pub fn element(
                 ConstExprKind::ElementOffset,
             )?;
             ElementMode::Active {
-                table: Some(t.remap(Item::Table, *table_index)?),
+                table: table_index.map(|i| t.remap(Item::Table, i)).transpose()?,
                 offset: &offset,
             }
         }

crates/wasmparser/src/readers/core/elements.rs
@@ -40,7 +40,7 @@ pub enum ElementKind<'a> {
     /// The element segment is active.
     Active {
         /// The index of the table being initialized.
-        table_index: u32,
+        table_index: Option<u32>,
         /// The initial expression of the element segment.
         offset_expr: ConstExpr<'a>,
     },
@@ -91,9 +91,9 @@ impl<'a> FromReader<'a> for Element<'a> {
             }
         } else {
             let table_index = if flags & 0b010 == 0 {
-                0
+                None
             } else {
-                reader.read_var_u32()?
+                Some(reader.read_var_u32()?)
             };
             let offset_expr = reader.read()?;
             ElementKind::Active {

crates/wasmparser/src/validator/core.rs
@@ -207,7 +207,7 @@ impl ModuleState {
                 table_index,
                 offset_expr,
             } => {
-                let table = self.module.table_at(table_index, offset)?;
+                let table = self.module.table_at(table_index.unwrap_or(0), offset)?;
                 if !self
                     .module
                     .matches(ValType::Ref(e.ty), ValType::Ref(table.element_type), types)

crates/wasmprinter/src/lib.rs
@@ -1170,9 +1170,10 @@ impl Printer {
                     table_index,
                     offset_expr,
                 } => {
-                    if *table_index != 0 {
+                    let table_index = table_index.unwrap_or(0);
+                    if table_index != 0 {
                         self.result.push_str(" (table ");
-                        self.print_idx(&state.core.table_names, *table_index)?;
+                        self.print_idx(&state.core.table_names, table_index)?;
                         self.result.push(')');
                     }
                     self.result.push(' ');

src/bin/wasm-tools/dump.rs
@@ -171,7 +171,7 @@ impl<'a> Dump<'a> {
                             table_index,
                             offset_expr,
                         } => {
-                            write!(me.state, " table[{}]", table_index)?;
+                            write!(me.state, " table[{:?}]", table_index)?;
                             me.print(offset_expr.get_binary_reader().original_position())?;
                             me.print_ops(offset_expr.get_operators_reader())?;
                             write!(me.state, "{} items", item_count)?;

tests/cli/dump/simple.wat.stdout
@@ -31,7 +31,7 @@
  0x3d | 00          | start function 0
  0x3e | 09 0f       | element section
  0x40 | 03          | 3 count
- 0x41 | 00          | element RefType { nullable: true, heap_type: Func } table[0]
+ 0x41 | 00          | element RefType { nullable: true, heap_type: Func } table[None]
  0x42 | 41 03       | i32_const value:3
  0x44 | 0b          | end
  0x45 | 01          | 1 items

commit 43b9c3884d4be55dbeb1e0505a0491749ea75f6a

wasm-mutate: Fix removing table 0 when an element references it
This fixes an accidental regression from #957 where wasm-mutate was
removing table 0 even when an element segment implicitly referenced it
by not having a table index listed.

crates/wasm-mutate/src/mutators/translate.rs
@@ -262,8 +262,10 @@ pub fn element(
                 &wasmparser::ValType::I32,
                 ConstExprKind::ElementOffset,
             )?;
+            let table_index = table_index.unwrap_or(0);
+            let table = t.remap(Item::Table, table_index)?;
             ElementMode::Active {
-                table: table_index.map(|i| t.remap(Item::Table, i)).transpose()?,
+                table: if table == 0 { None } else { Some(table) },
                 offset: &offset,
             }
         }