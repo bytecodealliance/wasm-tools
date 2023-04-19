e0029becdc0b0d66ceebad5960cce67f4271c510
Alex Crichton <alex@alexcrichton.com>
2023-04-19 12:46:45 -0700

    Add a `--skeleton` option to `wasm-tools print`
    Often when debugging larger wasm files I want to skip the data segments,
    element segments, and often entire function bodies. This commit adds a
    debugging option `--skeleton` to skip printing all these "big" items to
    ideally only print the "shape" of a module for easier manual inspection.
    An example issue I'm using this with is #986.

--- src/bin/wasm-tools/print.rs
+++ src/bin/wasm-tools/print.rs
@@ -11,6 +11,12 @@ pub struct Opts {
     /// as comments for debugging.
     #[clap(short, long)]
     print_offsets: bool,
+
+    /// Indicates that the "skeleton" of a module should be printed where items
+    /// such as function bodies, data segments, and element segments are
+    /// replaced with "..." instead of printing their actual contents.
+    #[clap(long)]
+    skeleton: bool,
 }
 
 impl Opts {
@@ -18,6 +24,7 @@ impl Opts {
         let wasm = self.io.parse_input_wasm()?;
         let mut printer = wasmprinter::Printer::new();
         printer.print_offsets(self.print_offsets);
+        printer.print_skeleton(self.skeleton);
         let wat = printer.print(&wasm)?;
         self.io.output(wasm_tools::Output::Wat(&wat))?;
         Ok(())
