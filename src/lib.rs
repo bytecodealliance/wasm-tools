f13d36a636785a9d356fc44c1b4b0b4f3a35542c
uint256_t <konndennsa@gmail.com>
2023-05-13 00:06:24 +0900

    Colorize section name in `dump` output (#1024)
    * Add --color
    
    * Recognize --color
    
    * Colorize sections
    
    * Some fixes
    
    * Fix color choice

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use anyhow::{bail, Context, Result};
 use std::fs::File;
 use std::io::{BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
+use termcolor::{Ansi, ColorChoice, NoColor, StandardStream, WriteColor};
 
 // Implements the verbosity flag for the CLI commands.
 #[derive(clap::Parser)]
@@ -48,6 +49,10 @@ pub struct InputOutput {
 
     #[clap(flatten)]
     verbosity: Verbosity,
+
+    /// Use colors in output.
+    #[clap(long = "color", short = 'c')]
+    color: Option<ColorChoice>,
 }
 
 #[derive(clap::Parser)]
@@ -89,8 +94,9 @@ impl InputOutput {
         self.output.output(bytes)
     }
 
-    pub fn output_writer(&self) -> Result<Box<dyn Write>> {
-        self.output.output_writer()
+    pub fn output_writer(&self) -> Result<Box<dyn WriteColor>> {
+        self.output
+            .output_writer(self.color.unwrap_or(ColorChoice::Auto))
     }
 
     pub fn output_path(&self) -> Option<&Path> {
@@ -150,10 +156,23 @@ impl OutputArg {
         self.output.as_deref()
     }
 
-    pub fn output_writer(&self) -> Result<Box<dyn Write>> {
+    pub fn output_writer(&self, color: ColorChoice) -> Result<Box<dyn WriteColor>> {
         match &self.output {
-            Some(output) => Ok(Box::new(BufWriter::new(File::create(&output)?))),
-            None => Ok(Box::new(std::io::stdout())),
+            Some(output) => {
+                let writer = BufWriter::new(File::create(&output)?);
+                if color == ColorChoice::AlwaysAnsi {
+                    Ok(Box::new(Ansi::new(writer)))
+                } else {
+                    Ok(Box::new(NoColor::new(writer)))
+                }
+            }
+            None => {
+                if color == ColorChoice::Auto && !atty::is(atty::Stream::Stdout) {
+                    Ok(Box::new(StandardStream::stdout(ColorChoice::Never)))
+                } else {
+                    Ok(Box::new(StandardStream::stdout(color)))
+                }
+            }
         }
     }
 }
