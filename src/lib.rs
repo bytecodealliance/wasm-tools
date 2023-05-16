8095211685baccae0162436400945c229a58396c
Jiaxiao Zhou <duibao55328@gmail.com>
2023-05-16 14:46:47 -0700

    feat: add trace-level log to verbose. (#1038)
    * Add trace-level log to verbose.
    
    * modify the verbose flag docs
    
    Signed-off-by: Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>
    
    ---------
    
    Signed-off-by: Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use termcolor::{Ansi, ColorChoice, NoColor, StandardStream, WriteColor};
 
 #[derive(clap::Parser)]
 pub struct GeneralOpts {
-    /// Use verbose output (-vv very verbose output).
+    /// Use verbose output (-v info, -vv debug, -vvv trace).
     #[clap(long = "verbose", short = 'v', action = clap::ArgAction::Count)]
     verbose: u8,
 
@@ -23,7 +23,8 @@ impl GeneralOpts {
         let default = match self.verbose {
             0 => "warn",
             1 => "info",
-            _ => "debug",
+            2 => "debug",
+            _ => "trace",
         };
 
         env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(default))
