e34e042d000251af5b0eb2315868f50b3e759f4c
Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>
2023-05-16 14:28:32 -0700

    Add trace-level log to verbose.

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,8 @@ impl GeneralOpts {
         let default = match self.verbose {
             0 => "warn",
             1 => "info",
-            _ => "debug",
+            2 => "debug",
+            _ => "trace",
         };
 
         env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(default))
