bytecodealliance/wasm-tools

commit e633e6f3a7640d38abf6790a8368f4999f1669db

GC: wasmparser: Subtypes (#1059)
* GC: wasmparser: Subtypes

Introducing the `SubType` struct:
```rust
pub struct SubType {
    pub is_final: bool,
    pub supertype_idxs: Vec<u32>,
    pub structural_type: StructuralType,
}
```

Here, `StructuralType` is an enum (previously named `Type`).

`supertype_idxs` is a vector of indexes of supertypes, limited to
at most one supertype as per GC MVP.

A valid subtype matches its supertypes. Supertypes cannot be final.

* Accept an edit suggestion: punctuation.

Co-authored-by: Nick Fitzgerald <fitzgen@gmail.com>

* Accept a suggestion: use .. in struct match expression

Co-authored-by: Nick Fitzgerald <fitzgen@gmail.com>

* Address feedback: `opcode`

* Address feedback: make supertype_idx an Option<u32>

* Address feedback: remove specific call order to encode subtype.

* Broke the tests. Fixed them now.

* Update matching logic.

* Add test cases.

* cargo fmt

* Update matching logic.

* Expand test coverage for subtypes

Expanded test coverage for different kinds of subtypes in gc-subtypes.wast and gc-subtypes-invalid.wast. Validation of mutability, nullability, alignment of subtypes and supertypes, and the equality of expanded param/result types to those of the parent are included.

* Expand test coverage for subtypes

* Expand test coverage for subtypes

* Update crates/wasm-encoder/src/core/types.rs

Co-authored-by: Nick Fitzgerald <fitzgen@gmail.com>

* Revert "Update crates/wasm-encoder/src/core/types.rs"

This reverts commit 376501894b32269d6bf6ba40992e6bab203f52a8.

* Make sure test cases for invalid subtypes are addressed in `gc-subtypes-invalid.wast`

* Remove outdated comment.

* Refactor `TypeSection::subtype()` removing unnecessary argument

* Encode supertypes as a vector (as per spec).

* Fix "duplicate identifier for field" for subtype fields

crates/wasmprinter/src/lib.rs
@@ -56,7 +56,7 @@ pub struct Printer {
 
 #[derive(Default)]
 struct CoreState {
-    types: Vec<Option<Type>>,
+    types: Vec<Option<SubType>>,
     funcs: u32,
     memories: u32,
     tags: u32,
@@ -645,7 +645,11 @@ impl Printer {
                 self.start_group("func");
                 self.print_func_type(states.last().unwrap(), &ty, None)?;
                 self.end_group();
-                Some(Type::Func(ty))
+                Some(SubType {
+                    is_final: false,
+                    supertype_idx: None,
+                    structural_type: StructuralType::Func(ty),
+                })
             }
             CoreType::Module(decls) => {
                 self.print_module_type(states, decls.into_vec())?;
@@ -658,33 +662,56 @@ impl Printer {
         Ok(())
     }
 
-    fn print_type(&mut self, state: &mut State, ty: wasmparser::Type) -> Result<()> {
+    fn print_type(&mut self, state: &mut State, ty: SubType) -> Result<()> {
         self.start_group("type ");
         self.print_name(&state.core.type_names, state.core.types.len() as u32)?;
         self.result.push(' ');
-        let ty = match ty {
-            Type::Func(ty) => {
-                self.start_group("func");
-                self.print_func_type(state, &ty, None)?;
-                self.end_group();
-                Type::Func(ty)
-            }
-            Type::Array(ty) => {
-                self.start_group("array");
-                self.print_array_type(&ty)?;
-                self.end_group();
-                Type::Array(ty)
-            }
-            Type::Struct(ty) => {
-                self.start_group("struct");
-                self.print_struct_type(&ty)?;
-                self.end_group();
-                Type::Struct(ty)
-            }
-        };
-        self.end_group(); // `type` itself
+        self.print_sub(state, &ty, None)?;
+        self.end_group(); // `type`
         state.core.types.push(Some(ty));
         Ok(())
+    }
+
+    fn print_sub(&mut self, state: &State, ty: &SubType, names_for: Option<u32>) -> Result<u32> {
+        let r = if ty.is_final || !ty.supertype_idx.is_none() {
+            self.start_group("sub");
+            self.print_sub_type(state, ty)?;
+            let r = self.print_structural(state, &ty.structural_type, names_for)?;
+            self.end_group(); // `sub`
+            r
+        } else {
+            self.print_structural(state, &ty.structural_type, names_for)?
+        };
+        Ok(r)
+    }
+
+    fn print_structural(
+        &mut self,
+        state: &State,
+        ty: &StructuralType,
+        names_for: Option<u32>,
+    ) -> Result<u32> {
+        let r = match &ty {
+            StructuralType::Func(ty) => {
+                self.start_group("func");
+                let r = self.print_func_type(state, ty, names_for)?;
+                self.end_group(); // `func`
+                r
+            }
+            StructuralType::Array(ty) => {
+                self.start_group("array");
+                let r = self.print_array_type(ty)?;
+                self.end_group(); // `array`
+                r
+            }
+            StructuralType::Struct(ty) => {
+                self.start_group("struct");
+                let r = self.print_struct_type(ty)?;
+                self.end_group(); // `struct`
+                r
+            }
+        };
+        Ok(r)
     }
 
     fn print_core_types(
@@ -720,9 +747,11 @@ impl Printer {
         self.print_core_type_ref(state, idx)?;
 
         match state.core.types.get(idx as usize) {
-            Some(Some(Type::Func(ty))) => self.print_func_type(state, ty, names_for).map(Some),
-            Some(Some(Type::Array(ty))) => self.print_array_type(ty).map(Some),
-            Some(Some(Type::Struct(ty))) => self.print_struct_type(ty).map(Some),
+            Some(Some(SubType {
+                structural_type: StructuralType::Func(ty),
+                ..
+            })) => self.print_func_type(state, ty, names_for).map(Some),
+            Some(Some(_)) => unreachable!("the core type must be a func"),
             Some(None) | None => Ok(None),
         }
     }
@@ -782,6 +811,18 @@ impl Printer {
             self.result.push_str(" (field");
             self.print_field_type(field)?;
             self.result.push(')');
+        }
+        Ok(0)
+    }
+
+    fn print_sub_type(&mut self, state: &State, ty: &SubType) -> Result<u32> {
+        self.result.push(' ');
+        if ty.is_final {
+            self.result.push_str("final ");
+        }
+        for idx in &ty.supertype_idx {
+            self.print_name(&state.core.type_names, *idx)?;
+            self.result.push(' ');
         }
         Ok(0)
     }

crates/wasm-compose/src/encoding.rs
@@ -462,7 +462,7 @@ impl<'a> TypeEncoder<'a> {
         let ty = self.0.type_from_id(id).unwrap();
 
         match ty {
-            Type::Func(_) | Type::Array(_) | Type::Struct(_) | Type::Instance(_) => {
+            Type::Sub(_) | Type::Instance(_) => {
                 unreachable!()
             }
             Type::Module(_) => self.module_type(encodable, types, id),

crates/wasm-encoder/src/core/types.rs
@@ -1,10 +1,52 @@
 use crate::{encode_section, Encode, Section, SectionId};
 
+/// Represents a subtype of possible other types in a WebAssembly module.
+#[derive(Debug, Clone)]
+pub struct SubType {
+    /// Is the subtype final.
+    pub is_final: bool,
+    /// The list of supertype indexes. As of GC MVP, there can be at most one supertype.
+    pub supertype_idx: Option<u32>,
+    /// The structural type of the subtype.
+    pub structural_type: StructuralType,
+}
+
+/// Represents a structural type in a WebAssembly module.
+#[derive(Debug, Clone)]
+pub enum StructuralType {
+    /// The type is for a function.
+    Func(FuncType),
+    /// The type is for an array.
+    Array(ArrayType),
+    /// The type is for a struct.
+    Struct(StructType),
+}
+
+/// Represents a type of a function in a WebAssembly module.
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct FuncType {
+    /// The combined parameters and result types.
+    params_results: Box<[ValType]>,
+    /// The number of parameter types.
+    len_params: usize,
+}
+
+/// Represents a type of an array in a WebAssembly module.
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct ArrayType(pub FieldType);
+
+/// Represents a type of a struct in a WebAssembly module.
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct StructType {
+    /// Struct fields.
+    pub fields: Box<[FieldType]>,
+}
+
 /// Field type in structural types (structs, arrays).
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub struct FieldType {
     /// Storage type of the field.
-    pub ty: StorageType,
+    pub element_type: StorageType,
     /// Is the field mutable.
     pub mutable: bool,
 }
@@ -41,6 +83,35 @@ pub enum ValType {
     /// generalization here is due to the implementation of the
     /// function-references proposal.
     Ref(RefType),
+}
+
+impl FuncType {
+    /// Creates a new [`FuncType`] from the given `params` and `results`.
+    pub fn new<P, R>(params: P, results: R) -> Self
+    where
+        P: IntoIterator<Item = ValType>,
+        R: IntoIterator<Item = ValType>,
+    {
+        let mut buffer = params.into_iter().collect::<Vec<_>>();
+        let len_params = buffer.len();
+        buffer.extend(results);
+        Self {
+            params_results: buffer.into(),
+            len_params,
+        }
+    }
+
+    /// Returns a shared slice to the parameter types of the [`FuncType`].
+    #[inline]
+    pub fn params(&self) -> &[ValType] {
+        &self.params_results[..self.len_params]
+    }
+
+    /// Returns a shared slice to the result types of the [`FuncType`].
+    #[inline]
+    pub fn results(&self) -> &[ValType] {
+        &self.params_results[self.len_params..]
+    }
 }
 
 impl ValType {
@@ -233,15 +304,16 @@ impl TypeSection {
     }
 
     /// Define an array type in this type section.
-    pub fn array(&mut self, ty: StorageType, mutable: bool) -> &mut Self {
-        self.field(ty, mutable)
+    pub fn array(&mut self, ty: &StorageType, mutable: bool) -> &mut Self {
+        self.bytes.push(0x5e);
+        self.field(ty, mutable);
+        self.num_added += 1;
+        self
     }
 
-    fn field(&mut self, ty: StorageType, mutable: bool) -> &mut Self {
-        self.bytes.push(0x5e);
+    fn field(&mut self, ty: &StorageType, mutable: bool) -> &mut Self {
         ty.encode(&mut self.bytes);
         self.bytes.push(mutable as u8);
-        self.num_added += 1;
         self
     }
 
@@ -250,9 +322,39 @@ impl TypeSection {
         self.bytes.push(0x5f);
         fields.len().encode(&mut self.bytes);
         for f in fields.iter() {
-            self.field(f.ty, f.mutable);
+            self.field(&f.element_type, f.mutable);
         }
         self.num_added += 1;
+        self
+    }
+
+    /// Define an explicit subtype in this type section.
+    pub fn subtype(&mut self, ty: &SubType) -> &mut Self {
+        // In the GC spec, supertypes is a vector, not an option.
+        let st = match ty.supertype_idx {
+            Some(idx) => vec![idx],
+            None => vec![],
+        };
+        if ty.is_final {
+            self.bytes.push(0x4e);
+            st.encode(&mut self.bytes);
+        } else if !st.is_empty() {
+            self.bytes.push(0x50);
+            st.encode(&mut self.bytes);
+        }
+
+        match &ty.structural_type {
+            StructuralType::Func(ty) => {
+                self.function(ty.params().iter().copied(), ty.results().iter().copied());
+            }
+            StructuralType::Array(ArrayType(ty)) => {
+                self.array(&ty.element_type, ty.mutable);
+            }
+            StructuralType::Struct(ty) => {
+                self.struct_(ty.fields.to_vec());
+            }
+        }
+
         self
     }
 }

crates/wasm-mutate/src/info.rs
@@ -1,6 +1,6 @@
 use crate::{
     module::{PrimitiveTypeInfo, TypeInfo},
-    Result,
+    Error, Result,
 };
 use std::collections::HashSet;
 use std::convert::TryFrom;
@@ -93,8 +93,13 @@ impl<'a> ModuleInfo<'a> {
 
                     // Save function types
                     for ty in reader {
-                        let typeinfo = TypeInfo::try_from(ty?).unwrap();
-                        info.types_map.push(typeinfo);
+                        if let Ok(st) = ty {
+                            if st.is_final || st.supertype_idx.is_some() {
+                                return Err(Error::unsupported("GC types not supported yet"));
+                            }
+                            let typeinfo = TypeInfo::try_from(st.structural_type).unwrap();
+                            info.types_map.push(typeinfo);
+                        }
                     }
                 }
                 Payload::ImportSection(reader) => {

crates/wasm-mutate/src/module.rs
@@ -49,12 +49,12 @@ impl From<wasmparser::RefType> for PrimitiveTypeInfo {
     }
 }
 
-impl TryFrom<wasmparser::Type> for TypeInfo {
+impl TryFrom<wasmparser::StructuralType> for TypeInfo {
     type Error = Error;
 
-    fn try_from(value: wasmparser::Type) -> Result<Self> {
+    fn try_from(value: wasmparser::StructuralType) -> Result<Self> {
         match value {
-            wasmparser::Type::Func(ft) => Ok(TypeInfo::Func(FuncInfo {
+            wasmparser::StructuralType::Func(ft) => Ok(TypeInfo::Func(FuncInfo {
                 params: ft
                     .params()
                     .iter()
@@ -66,10 +66,10 @@ impl TryFrom<wasmparser::Type> for TypeInfo {
                     .map(|&t| PrimitiveTypeInfo::from(t))
                     .collect(),
             })),
-            wasmparser::Type::Array(_) => {
+            wasmparser::StructuralType::Array(_) => {
                 Err(Error::unsupported("Array types are not supported yet."))
             }
-            wasmparser::Type::Struct(_) => {
+            wasmparser::StructuralType::Struct(_) => {
                 Err(Error::unsupported("Struct types are not supported yet."))
             }
         }

crates/wasm-mutate/src/mutators/add_type.rs
@@ -56,8 +56,8 @@ impl Mutator for AddTypeMutator {
             // Copy the existing types section over into the encoder.
             let reader = wasmparser::TypeSectionReader::new(old_types.data, 0)?;
             for ty in reader {
-                match ty? {
-                    wasmparser::Type::Func(ty) => {
+                match ty?.structural_type {
+                    wasmparser::StructuralType::Func(ty) => {
                         let params = ty
                             .params()
                             .iter()
@@ -72,10 +72,10 @@ impl Mutator for AddTypeMutator {
                             .collect::<Result<Vec<_>, _>>()?;
                         types.function(params, results);
                     }
-                    wasmparser::Type::Array(_) => {
+                    wasmparser::StructuralType::Array(_) => {
                         return Err(Error::unsupported("Array types are not supported yet."));
                     }
-                    wasmparser::Type::Struct(_) => {
+                    wasmparser::StructuralType::Struct(_) => {
                         return Err(Error::unsupported("Struct types are not supported yet."));
                     }
                 }

crates/wasm-mutate/src/mutators/remove_item.rs
@@ -136,7 +136,7 @@ impl RemoveItem {
                         0,
                         TypeSectionReader::new(section.data, 0)?,
                         Item::Type,
-                        |me, ty, section| me.translate_type_def(ty, section),
+                        |me, ty, section| me.translate_type_def(ty.structural_type, section),
                     )?;
                 },
 

crates/wasm-mutate/src/mutators/translate.rs
@@ -1,6 +1,6 @@
 use crate::{Error, Result};
 use wasm_encoder::*;
-use wasmparser::{DataKind, ElementKind, FunctionBody, Global, Operator, Type};
+use wasmparser::{DataKind, ElementKind, FunctionBody, Global, Operator, StructuralType};
 
 #[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
 pub enum Item {
@@ -26,7 +26,7 @@ pub enum ConstExprKind {
 pub trait Translator {
     fn as_obj(&mut self) -> &mut dyn Translator;
 
-    fn translate_type_def(&mut self, ty: Type, s: &mut TypeSection) -> Result<()> {
+    fn translate_type_def(&mut self, ty: StructuralType, s: &mut TypeSection) -> Result<()> {
         type_def(self.as_obj(), ty, s)
     }
 
@@ -122,9 +122,9 @@ impl Translator for DefaultTranslator {
     }
 }
 
-pub fn type_def(t: &mut dyn Translator, ty: Type, s: &mut TypeSection) -> Result<()> {
+pub fn type_def(t: &mut dyn Translator, ty: StructuralType, s: &mut TypeSection) -> Result<()> {
     match ty {
-        Type::Func(f) => {
+        StructuralType::Func(f) => {
             s.function(
                 f.params()
                     .iter()
@@ -137,8 +137,8 @@ pub fn type_def(t: &mut dyn Translator, ty: Type, s: &mut TypeSection) -> Result
             );
             Ok(())
         }
-        Type::Array(_) => Err(Error::unsupported("Array types are not supported yet.")),
-        Type::Struct(_) => Err(Error::unsupported("Struct types are not supported yet.")),
+        StructuralType::Array(_) => Err(Error::unsupported("Array types are not supported yet.")),
+        StructuralType::Struct(_) => Err(Error::unsupported("Struct types are not supported yet.")),
     }
 }
 

commit 59b39fca33ed7270f5f47d2eb9179da138b73336

Don't panic in wasmprinter with non-function types
This fixes an accidental regression from #1059 where an invalid wasm
file was causing a panic in wasmprinter, and wasmprinter shouldn't be
panicking on invalid wasm files.

crates/wasmprinter/src/lib.rs
@@ -751,8 +751,7 @@ impl Printer {
                 structural_type: StructuralType::Func(ty),
                 ..
             })) => self.print_func_type(state, ty, names_for).map(Some),
-            Some(Some(_)) => unreachable!("the core type must be a func"),
-            Some(None) | None => Ok(None),
+            Some(Some(_)) | Some(None) | None => Ok(None),
         }
     }
 

crates/wasmprinter/tests/all.rs
@@ -264,3 +264,15 @@ fn offsets_and_lines_smoke_test() {
 
     assert_eq!(actual, expected);
 }
+
+#[test]
+fn no_panic_non_func_type() {
+    let bytes = wat::parse_str(
+        "(module
+            (type (struct))
+            (func (type 0))
+        )",
+    )
+    .unwrap();
+    wasmprinter::print_bytes(&bytes).unwrap();
+}