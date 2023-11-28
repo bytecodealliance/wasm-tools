bytecodealliance/wasm-tools

commit 230ce7f43f23b471c232483ba700bbd724906e28

Canonicalize types earlier in validation (#1299)
* Canonicalize types earlier in validation

This commit refactors the internals of validation to centralize
canonicalization in one location. This means that stages such as
operator validation no longer need to perform canonicalization.
Additionally types are always stored in their canonical forms.

The refactoring here is done when a `RecGroup` is inserted into a
`TypeList`. The rec-group-local offsets are used to intern the group but
when actually adding to a `TypeList` all rec-group-local offsets are
updated to an ID-based form. This is then plumbed in many other
locations as well by changing many `t: ValType` validators to `t: &mut
ValType` to internally canonicalize.

This is originally motivated by trying to update Wasmtime to the latest
`wasmparser` but many APIs which previously returned `&FuncType` for
example returned `FuncType` instead due to the
canonicalization-on-the-fly. This change means that the old
`&FuncType`-style APIs can return because types are always canonicalized
at-rest.

This change additionally changes the behavior of one test. It was
previously considered valid and is now considered valid. I've confirmed
that `wasm-opt` considers the test invalid, so I've updated the test to
`assert_invalid` instead.

* Fix test compile

* Remove unused arguments

* Review comments

crates/wasmparser/src/validator/operators.rs
@@ -24,8 +24,8 @@
 
 use crate::{
     limits::MAX_WASM_FUNCTION_LOCALS, BinaryReaderError, BlockType, BrTable, HeapType, Ieee32,
-    Ieee64, MemArg, PackedIndex, RefType, Result, UnpackedIndex, ValType, VisitOperator,
-    WasmFeatures, WasmFuncType, WasmModuleResources, V128,
+    Ieee64, MemArg, RefType, Result, UnpackedIndex, ValType, VisitOperator, WasmFeatures,
+    WasmFuncType, WasmModuleResources, V128,
 };
 use std::ops::{Deref, DerefMut};
 
@@ -245,8 +245,7 @@ impl OperatorValidator {
         }
         .func_type_at(ty)?
         .inputs();
-        for mut ty in params {
-            resources.canonicalize_valtype(&mut ty);
+        for ty in params {
             ret.locals.define(1, ty);
             ret.local_inits.push(true);
         }
@@ -279,8 +278,7 @@ impl OperatorValidator {
         mut ty: ValType,
         resources: &impl WasmModuleResources,
     ) -> Result<()> {
-        resources.check_value_type(ty, &self.features, offset)?;
-        resources.canonicalize_valtype(&mut ty);
+        resources.check_value_type(&mut ty, &self.features, offset)?;
         if count == 0 {
             return Ok(());
         }
@@ -711,7 +709,7 @@ where
     }
 
     /// Validates a block type, primarily with various in-flight proposals.
-    fn check_block_type(&self, ty: BlockType) -> Result<()> {
+    fn check_block_type(&self, ty: &mut BlockType) -> Result<()> {
         match ty {
             BlockType::Empty => Ok(()),
             BlockType::Type(t) => self
@@ -725,7 +723,7 @@ where
                          when multi-value is not enabled",
                     );
                 }
-                self.func_type_at(idx)?;
+                self.func_type_at(*idx)?;
                 Ok(())
             }
         }
@@ -734,7 +732,7 @@ where
     /// Validates a `call` instruction, ensuring that the function index is
     /// in-bounds and the right types are on the stack to call the function.
     fn check_call(&mut self, function_index: u32) -> Result<()> {
-        let ty = match self.resources.type_index_of_function(function_index) {
+        let ty = match self.resources.type_of_function(function_index) {
             Some(i) => i,
             None => {
                 bail!(
@@ -746,7 +744,7 @@ where
         self.check_call_ty(ty)
     }
 
-    fn check_call_ty(&mut self, type_index: u32) -> Result<()> {
+    fn check_call_type_index(&mut self, type_index: u32) -> Result<()> {
         let ty = match self.resources.func_type_at(type_index) {
             Some(i) => i,
             None => {
@@ -756,7 +754,11 @@ where
                 );
             }
         };
-        for ty in ty.clone().inputs().rev() {
+        self.check_call_ty(ty)
+    }
+
+    fn check_call_ty(&mut self, ty: &R::FuncType) -> Result<()> {
+        for ty in ty.inputs().rev() {
             debug_assert_type_indices_are_ids(ty);
             self.pop_operand(Some(ty))?;
         }
@@ -968,13 +970,13 @@ where
         Ok(())
     }
 
-    fn func_type_at(&self, at: u32) -> Result<R::FuncType> {
+    fn func_type_at(&self, at: u32) -> Result<&'resources R::FuncType> {
         self.resources
             .func_type_at(at)
             .ok_or_else(|| format_err!(self.offset, "unknown type: type index out of bounds"))
     }
 
-    fn tag_at(&self, at: u32) -> Result<R::FuncType> {
+    fn tag_at(&self, at: u32) -> Result<&'resources R::FuncType> {
         self.resources
             .tag_at(at)
             .ok_or_else(|| format_err!(self.offset, "unknown tag {}: tag index out of bounds", at))
@@ -990,10 +992,7 @@ where
     fn results(&self, ty: BlockType) -> Result<impl PreciseIterator<Item = ValType> + 'resources> {
         Ok(match ty {
             BlockType::Empty => Either::B(None.into_iter()),
-            BlockType::Type(mut t) => {
-                self.resources.canonicalize_valtype(&mut t);
-                Either::B(Some(t).into_iter())
-            }
+            BlockType::Type(t) => Either::B(Some(t).into_iter()),
             BlockType::FuncType(t) => Either::A(self.func_type_at(t)?.outputs()),
         })
     }
@@ -1108,24 +1107,24 @@ where
         self.unreachable()?;
         Ok(())
     }
-    fn visit_block(&mut self, ty: BlockType) -> Self::Output {
-        self.check_block_type(ty)?;
+    fn visit_block(&mut self, mut ty: BlockType) -> Self::Output {
+        self.check_block_type(&mut ty)?;
         for ty in self.params(ty)?.rev() {
             self.pop_operand(Some(ty))?;
         }
         self.push_ctrl(FrameKind::Block, ty)?;
         Ok(())
     }
-    fn visit_loop(&mut self, ty: BlockType) -> Self::Output {
-        self.check_block_type(ty)?;
+    fn visit_loop(&mut self, mut ty: BlockType) -> Self::Output {
+        self.check_block_type(&mut ty)?;
         for ty in self.params(ty)?.rev() {
             self.pop_operand(Some(ty))?;
         }
         self.push_ctrl(FrameKind::Loop, ty)?;
         Ok(())
     }
-    fn visit_if(&mut self, ty: BlockType) -> Self::Output {
-        self.check_block_type(ty)?;
+    fn visit_if(&mut self, mut ty: BlockType) -> Self::Output {
+        self.check_block_type(&mut ty)?;
         self.pop_operand(Some(ValType::I32))?;
         for ty in self.params(ty)?.rev() {
             self.pop_operand(Some(ty))?;
@@ -1141,8 +1140,8 @@ where
         self.push_ctrl(FrameKind::Else, frame.block_type)?;
         Ok(())
     }
-    fn visit_try(&mut self, ty: BlockType) -> Self::Output {
-        self.check_block_type(ty)?;
+    fn visit_try(&mut self, mut ty: BlockType) -> Self::Output {
+        self.check_block_type(&mut ty)?;
         for ty in self.params(ty)?.rev() {
             self.pop_operand(Some(ty))?;
         }
@@ -1315,23 +1314,13 @@ where
     }
     fn visit_call_ref(&mut self, type_index: u32) -> Self::Output {
         let unpacked_index = UnpackedIndex::Module(type_index);
-        let hty = HeapType::Concrete(unpacked_index);
-        self.resources
-            .check_heap_type(hty, &self.features, self.offset)?;
+        let mut hty = HeapType::Concrete(unpacked_index);
+        self.resources.check_heap_type(&mut hty, self.offset)?;
         // If `None` is popped then that means a "bottom" type was popped which
         // is always considered equivalent to the `hty` tag.
         if let Some(rt) = self.pop_ref()? {
-            let expected = RefType::concrete(
-                true,
-                unpacked_index.pack().ok_or_else(|| {
-                    BinaryReaderError::new(
-                        "implementation limit: type index too large",
-                        self.offset,
-                    )
-                })?,
-            );
-            let mut expected = ValType::Ref(expected);
-            self.resources.canonicalize_valtype(&mut expected);
+            let expected = RefType::new(true, hty).expect("hty should be previously validated");
+            let expected = ValType::Ref(expected);
             if !self.resources.is_subtype(ValType::Ref(rt), expected) {
                 bail!(
                     self.offset,
@@ -1339,7 +1328,7 @@ where
                 );
             }
         }
-        self.check_call_ty(type_index)
+        self.check_call_type_index(type_index)
     }
     fn visit_return_call_ref(&mut self, type_index: u32) -> Self::Output {
         self.visit_call_ref(type_index)?;
@@ -1408,8 +1397,7 @@ where
     }
     fn visit_typed_select(&mut self, mut ty: ValType) -> Self::Output {
         self.resources
-            .check_value_type(ty, &self.features, self.offset)?;
-        self.resources.canonicalize_valtype(&mut ty);
+            .check_value_type(&mut ty, &self.features, self.offset)?;
         self.pop_operand(Some(ValType::I32))?;
         self.pop_operand(Some(ty))?;
         self.pop_operand(Some(ty))?;
@@ -2254,13 +2242,12 @@ where
     fn visit_atomic_fence(&mut self) -> Self::Output {
         Ok(())
     }
-    fn visit_ref_null(&mut self, heap_type: HeapType) -> Self::Output {
+    fn visit_ref_null(&mut self, mut heap_type: HeapType) -> Self::Output {
         self.resources
-            .check_heap_type(heap_type, &self.features, self.offset)?;
-        let mut ty = ValType::Ref(
+            .check_heap_type(&mut heap_type, self.offset)?;
+        let ty = ValType::Ref(
             RefType::new(true, heap_type).expect("existing heap types should be within our limits"),
         );
-        self.resources.canonicalize_valtype(&mut ty);
         self.push_operand(ty)?;
         Ok(())
     }
@@ -2331,8 +2318,8 @@ where
         Ok(())
     }
     fn visit_ref_func(&mut self, function_index: u32) -> Self::Output {
-        let type_index = match self.resources.type_index_of_function(function_index) {
-            Some(idx) => idx,
+        let type_id = match self.resources.type_id_of_function(function_index) {
+            Some(id) => id,
             None => bail!(
                 self.offset,
                 "unknown function {}: function index out of bounds",
@@ -2343,18 +2330,13 @@ where
             bail!(self.offset, "undeclared function reference");
         }
 
-        // FIXME(#924) this should not be conditional based on enabled
-        // proposals.
-        if self.features.function_references {
-            let index = PackedIndex::from_module_index(type_index).ok_or_else(|| {
+        let index = UnpackedIndex::Id(type_id);
+        let ty = ValType::Ref(
+            RefType::new(false, HeapType::Concrete(index)).ok_or_else(|| {
                 BinaryReaderError::new("implementation limit: type index too large", self.offset)
-            })?;
-            let mut ty = ValType::Ref(RefType::concrete(false, index));
-            self.resources.canonicalize_valtype(&mut ty);
-            self.push_operand(ty)?;
-        } else {
-            self.push_operand(ValType::FUNCREF)?;
-        }
+            })?,
+        );
+        self.push_operand(ty)?;
         Ok(())
     }
     fn visit_v128_load(&mut self, memarg: MemArg) -> Self::Output {

crates/wasmparser/Cargo.toml
@@ -13,7 +13,6 @@ edition.workspace = true
 exclude = ["benches/*.wasm"]
 
 [dependencies]
-hashbrown = { version = "0.14.2", default-features = false, features = ["ahash"] }
 indexmap = { workspace = true }
 semver = { workspace = true }
 

crates/wasmparser/src/readers/core/types.rs
@@ -13,14 +13,14 @@
  * limitations under the License.
  */
 
-use std::fmt::{self, Debug, Write};
-
 use crate::limits::{
     MAX_WASM_FUNCTION_PARAMS, MAX_WASM_FUNCTION_RETURNS, MAX_WASM_STRUCT_FIELDS,
     MAX_WASM_SUPERTYPES, MAX_WASM_TYPES,
 };
 use crate::types::CoreTypeId;
 use crate::{BinaryReader, BinaryReaderError, FromReader, Result, SectionLimited};
+use std::fmt::{self, Debug, Write};
+use std::hash::{Hash, Hasher};
 
 mod matches;
 pub(crate) use self::matches::{Matches, WithRecGroup};
@@ -384,6 +384,20 @@ impl RecGroup {
     }
 }
 
+impl Hash for RecGroup {
+    fn hash<H: Hasher>(&self, hasher: &mut H) {
+        self.types().hash(hasher)
+    }
+}
+
+impl PartialEq for RecGroup {
+    fn eq(&self, other: &RecGroup) -> bool {
+        self.types() == other.types()
+    }
+}
+
+impl Eq for RecGroup {}
+
 /// Represents a subtype of possible other types in a WebAssembly module.
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct SubType {
@@ -415,6 +429,35 @@ impl SubType {
     /// Does not check finality or whether there is a supertype.
     pub fn unwrap_struct(&self) -> &StructType {
         self.composite_type.unwrap_struct()
+    }
+
+    /// Maps any `UnpackedIndex` via the specified closure.
+    pub(crate) fn remap_indices(
+        &mut self,
+        f: &mut dyn FnMut(&mut PackedIndex) -> Result<()>,
+    ) -> Result<()> {
+        if let Some(idx) = &mut self.supertype_idx {
+            f(idx)?;
+        }
+        match &mut self.composite_type {
+            CompositeType::Func(ty) => {
+                for ty in ty.params_mut() {
+                    ty.remap_indices(f)?;
+                }
+                for ty in ty.results_mut() {
+                    ty.remap_indices(f)?;
+                }
+            }
+            CompositeType::Array(ty) => {
+                ty.0.remap_indices(f)?;
+            }
+            CompositeType::Struct(ty) => {
+                for field in ty.fields.iter_mut() {
+                    field.remap_indices(f)?;
+                }
+            }
+        }
+        Ok(())
     }
 }
 
@@ -562,6 +605,19 @@ pub struct FieldType {
     pub mutable: bool,
 }
 
+impl FieldType {
+    /// Maps any `UnpackedIndex` via the specified closure.
+    pub(crate) fn remap_indices(
+        &mut self,
+        f: &mut dyn FnMut(&mut PackedIndex) -> Result<()>,
+    ) -> Result<()> {
+        match &mut self.element_type {
+            StorageType::I8 | StorageType::I16 => Ok(()),
+            StorageType::Val(ty) => ty.remap_indices(f),
+        }
+    }
+}
+
 /// Represents storage types introduced in the GC spec for array and struct fields.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum StorageType {
@@ -639,6 +695,23 @@ impl ValType {
             Self::I32 | Self::I64 | Self::F32 | Self::F64 | Self::V128 => true,
             Self::Ref(rt) => rt.is_nullable(),
         }
+    }
+
+    /// Maps any `UnpackedIndex` via the specified closure.
+    pub(crate) fn remap_indices(
+        &mut self,
+        map: &mut dyn FnMut(&mut PackedIndex) -> Result<()>,
+    ) -> Result<()> {
+        match self {
+            ValType::Ref(r) => {
+                if let Some(mut idx) = r.type_index() {
+                    map(&mut idx)?;
+                    *r = RefType::concrete(r.is_nullable(), idx);
+                }
+            }
+            ValType::I32 | ValType::I64 | ValType::F32 | ValType::F64 | ValType::V128 => {}
+        }
+        Ok(())
     }
 }
 

crates/wasmparser/src/resources.rs
@@ -13,6 +13,7 @@
  * limitations under the License.
  */
 
+use crate::types::CoreTypeId;
 use crate::{
     BinaryReaderError, FuncType, GlobalType, HeapType, MemoryType, RefType, TableType, ValType,
     WasmFeatures,
@@ -48,7 +49,7 @@ pub trait WasmFuncType: Clone {
     fn output_at(&self, at: u32) -> Option<ValType>;
 
     /// Returns the list of inputs as an iterator.
-    fn inputs(self) -> WasmFuncTypeInputs<Self>
+    fn inputs(&self) -> WasmFuncTypeInputs<'_, Self>
     where
         Self: Sized,
     {
@@ -60,7 +61,7 @@ pub trait WasmFuncType: Clone {
     }
 
     /// Returns the list of outputs as an iterator.
-    fn outputs(self) -> WasmFuncTypeOutputs<Self>
+    fn outputs(&self) -> WasmFuncTypeOutputs<'_, Self>
     where
         Self: Sized,
     {
@@ -92,14 +93,14 @@ where
 
 /// Iterator over the inputs of a Wasm function type.
 #[derive(Clone)]
-pub struct WasmFuncTypeInputs<T> {
+pub struct WasmFuncTypeInputs<'a, T> {
     /// The iterated-over function type.
-    func_type: T,
+    func_type: &'a T,
     /// The range we're iterating over.
     range: Range<u32>,
 }
 
-impl<T> Iterator for WasmFuncTypeInputs<T>
+impl<T> Iterator for WasmFuncTypeInputs<'_, T>
 where
     T: WasmFuncType,
 {
@@ -116,7 +117,7 @@ where
     }
 }
 
-impl<T> DoubleEndedIterator for WasmFuncTypeInputs<T>
+impl<T> DoubleEndedIterator for WasmFuncTypeInputs<'_, T>
 where
     T: WasmFuncType,
 {
@@ -127,7 +128,7 @@ where
     }
 }
 
-impl<T> ExactSizeIterator for WasmFuncTypeInputs<T>
+impl<T> ExactSizeIterator for WasmFuncTypeInputs<'_, T>
 where
     T: WasmFuncType,
 {
@@ -138,14 +139,14 @@ where
 
 /// Iterator over the outputs of a Wasm function type.
 #[derive(Clone)]
-pub struct WasmFuncTypeOutputs<T> {
+pub struct WasmFuncTypeOutputs<'a, T> {
     /// The iterated-over function type.
-    func_type: T,
+    func_type: &'a T,
     /// The range we're iterating over.
     range: Range<u32>,
 }
 
-impl<T> Iterator for WasmFuncTypeOutputs<T>
+impl<T> Iterator for WasmFuncTypeOutputs<'_, T>
 where
     T: WasmFuncType,
 {
@@ -162,7 +163,7 @@ where
     }
 }
 
-impl<T> DoubleEndedIterator for WasmFuncTypeOutputs<T>
+impl<T> DoubleEndedIterator for WasmFuncTypeOutputs<'_, T>
 where
     T: WasmFuncType,
 {
@@ -173,7 +174,7 @@ where
     }
 }
 
-impl<T> ExactSizeIterator for WasmFuncTypeOutputs<T>
+impl<T> ExactSizeIterator for WasmFuncTypeOutputs<'_, T>
 where
     T: WasmFuncType,
 {
@@ -205,7 +206,7 @@ pub trait WasmModuleResources {
     /// Returns the tag at given index.
     ///
     /// The tag's function type must be canonicalized.
-    fn tag_at(&self, at: u32) -> Option<Self::FuncType>;
+    fn tag_at(&self, at: u32) -> Option<&Self::FuncType>;
 
     /// Returns the global variable at given index.
     ///
@@ -215,20 +216,16 @@ pub trait WasmModuleResources {
     /// Returns the `FuncType` associated with the given type index.
     ///
     /// The function type must be canonicalized.
-    fn func_type_at(&self, type_idx: u32) -> Option<Self::FuncType>;
+    fn func_type_at(&self, type_idx: u32) -> Option<&Self::FuncType>;
 
-    /// Returns the type index associated with the given function
+    /// Returns the type id associated with the given function
     /// index.
-    ///
-    /// ```ignore
-    /// type_of_function = func_type_at(type_index_of_function)
-    /// ```
-    fn type_index_of_function(&self, func_idx: u32) -> Option<u32>;
+    fn type_id_of_function(&self, func_idx: u32) -> Option<CoreTypeId>;
 
     /// Returns the `FuncType` associated with the given function index.
     ///
     /// The function type must be canonicalized.
-    fn type_of_function(&self, func_idx: u32) -> Option<Self::FuncType>;
+    fn type_of_function(&self, func_idx: u32) -> Option<&Self::FuncType>;
 
     /// Returns the element type at the given index.
     ///
@@ -238,39 +235,41 @@ pub trait WasmModuleResources {
     /// Is `a` a subtype of `b`?
     fn is_subtype(&self, a: ValType, b: ValType) -> bool;
 
-    /// Check a value type.
+    /// Check and canonicalize a value type.
     ///
-    /// This requires using func_type_at to check references
+    /// This will validate that `t` is valid under the `features` provided and
+    /// then additionally validate the structure of `t`. For example any type
+    /// references that `t` makes are validated and canonicalized.
     fn check_value_type(
         &self,
-        t: ValType,
-        features: &WasmFeatures,
-        offset: usize,
-    ) -> Result<(), BinaryReaderError>;
-
-    /// Checks that a `HeapType` is valid, notably its function index if one is
-    /// used.
-    fn check_heap_type(
-        &self,
-        heap_type: HeapType,
+        t: &mut ValType,
         features: &WasmFeatures,
         offset: usize,
     ) -> Result<(), BinaryReaderError> {
-        // Delegate to the generic value type validation which will have the
-        // same validity checks.
-        self.check_value_type(
-            RefType::new(true, heap_type)
-                .ok_or_else(|| {
-                    BinaryReaderError::new(
-                        "heap type index beyond this crate's implementation limits",
-                        offset,
-                    )
-                })?
-                .into(),
-            features,
-            offset,
-        )
+        features
+            .check_value_type(*t)
+            .map_err(|s| BinaryReaderError::new(s, offset))?;
+        match t {
+            ValType::Ref(r) => {
+                let nullable = r.is_nullable();
+                let mut hty = r.heap_type();
+                self.check_heap_type(&mut hty, offset)?;
+                *r = RefType::new(nullable, hty).unwrap();
+                Ok(())
+            }
+            ValType::I32 | ValType::I64 | ValType::F32 | ValType::F64 | ValType::V128 => Ok(()),
+        }
     }
+
+    /// Checks that a `HeapType` is valid and then additionally place it in its
+    /// canonical form.
+    ///
+    /// Similar to `check_value_type` but for heap types.
+    fn check_heap_type(
+        &self,
+        heap_type: &mut HeapType,
+        offset: usize,
+    ) -> Result<(), BinaryReaderError>;
 
     /// Returns the number of elements.
     fn element_count(&self) -> u32;
@@ -281,9 +280,6 @@ pub trait WasmModuleResources {
     /// Returns whether the function index is referenced in the module anywhere
     /// outside of the start/function sections.
     fn is_function_referenced(&self, idx: u32) -> bool;
-
-    /// Canonicalize the given value type in place.
-    fn canonicalize_valtype(&self, valtype: &mut ValType);
 }
 
 impl<T> WasmModuleResources for &'_ T
@@ -298,28 +294,23 @@ where
     fn memory_at(&self, at: u32) -> Option<MemoryType> {
         T::memory_at(self, at)
     }
-    fn tag_at(&self, at: u32) -> Option<Self::FuncType> {
+    fn tag_at(&self, at: u32) -> Option<&Self::FuncType> {
         T::tag_at(self, at)
     }
     fn global_at(&self, at: u32) -> Option<GlobalType> {
         T::global_at(self, at)
     }
-    fn func_type_at(&self, at: u32) -> Option<Self::FuncType> {
+    fn func_type_at(&self, at: u32) -> Option<&Self::FuncType> {
         T::func_type_at(self, at)
     }
-    fn type_index_of_function(&self, func_idx: u32) -> Option<u32> {
-        T::type_index_of_function(self, func_idx)
+    fn type_id_of_function(&self, func_idx: u32) -> Option<CoreTypeId> {
+        T::type_id_of_function(self, func_idx)
     }
-    fn type_of_function(&self, func_idx: u32) -> Option<Self::FuncType> {
+    fn type_of_function(&self, func_idx: u32) -> Option<&Self::FuncType> {
         T::type_of_function(self, func_idx)
     }
-    fn check_value_type(
-        &self,
-        t: ValType,
-        features: &WasmFeatures,
-        offset: usize,
-    ) -> Result<(), BinaryReaderError> {
-        T::check_value_type(self, t, features, offset)
+    fn check_heap_type(&self, t: &mut HeapType, offset: usize) -> Result<(), BinaryReaderError> {
+        T::check_heap_type(self, t, offset)
     }
     fn element_type_at(&self, at: u32) -> Option<RefType> {
         T::element_type_at(self, at)
@@ -336,9 +327,6 @@ where
     }
     fn is_function_referenced(&self, idx: u32) -> bool {
         T::is_function_referenced(self, idx)
-    }
-    fn canonicalize_valtype(&self, valtype: &mut ValType) {
-        T::canonicalize_valtype(self, valtype)
     }
 }
 
@@ -356,7 +344,7 @@ where
         T::memory_at(self, at)
     }
 
-    fn tag_at(&self, at: u32) -> Option<Self::FuncType> {
+    fn tag_at(&self, at: u32) -> Option<&Self::FuncType> {
         T::tag_at(self, at)
     }
 
@@ -364,25 +352,20 @@ where
         T::global_at(self, at)
     }
 
-    fn func_type_at(&self, type_idx: u32) -> Option<Self::FuncType> {
+    fn func_type_at(&self, type_idx: u32) -> Option<&Self::FuncType> {
         T::func_type_at(self, type_idx)
     }
 
-    fn type_index_of_function(&self, func_idx: u32) -> Option<u32> {
-        T::type_index_of_function(self, func_idx)
+    fn type_id_of_function(&self, func_idx: u32) -> Option<CoreTypeId> {
+        T::type_id_of_function(self, func_idx)
     }
 
-    fn type_of_function(&self, func_idx: u32) -> Option<Self::FuncType> {
+    fn type_of_function(&self, func_idx: u32) -> Option<&Self::FuncType> {
         T::type_of_function(self, func_idx)
     }
 
-    fn check_value_type(
-        &self,
-        t: ValType,
-        features: &WasmFeatures,
-        offset: usize,
-    ) -> Result<(), BinaryReaderError> {
-        T::check_value_type(self, t, features, offset)
+    fn check_heap_type(&self, t: &mut HeapType, offset: usize) -> Result<(), BinaryReaderError> {
+        T::check_heap_type(self, t, offset)
     }
 
     fn element_type_at(&self, at: u32) -> Option<RefType> {
@@ -403,10 +386,6 @@ where
 
     fn is_function_referenced(&self, idx: u32) -> bool {
         T::is_function_referenced(self, idx)
-    }
-
-    fn canonicalize_valtype(&self, valtype: &mut ValType) {
-        T::canonicalize_valtype(self, valtype)
     }
 }
 

commit 69f89f5606ada50496c74fd3acc4bfe09a34c4a8

Gate types coming through `ref.null`
This fixes a regression from #1299 where `ref.null array`, a wasm GC
instruction, wasn't being properly gated by the `gc` feature.

crates/wasmparser/src/validator/operators.rs
@@ -2243,6 +2243,11 @@ where
         Ok(())
     }
     fn visit_ref_null(&mut self, mut heap_type: HeapType) -> Self::Output {
+        if let Some(ty) = RefType::new(true, heap_type) {
+            self.features
+                .check_ref_type(ty)
+                .map_err(|e| BinaryReaderError::new(e, self.offset))?;
+        }
         self.resources
             .check_heap_type(&mut heap_type, self.offset)?;
         let ty = ValType::Ref(

tests/local/missing-features/reference-types/gc.wast
@@ -28,3 +28,6 @@
 (assert_invalid
   (module (func (block (result i31ref) unreachable)))
   "heap types not supported without the gc feature")
+(assert_invalid
+  (module (func ref.null array drop))
+  "heap types not supported without the gc feature")