3998d10723b4befe037ccb3fbb56aa201e614b5e
Alex Crichton <alex@alexcrichton.com>
2023-12-06 10:35:08 -0600

    Parse and print `//`-style comments as docs (#1313)
    * Parse and print `//`-style comments as docs
    
    This updates `wit-parser` to account for WebAssembly/component-model#281.
    Doc comments are now considered to be all forms of comments and are
    collected in the `Docs` structure. The structure already stripped all
    leading `/` characters so it works for either `///` or `//`.
    Additionally docs are now printed with `//` instead of `///`.
    
    * Update test expectations

--- crates/wasm-compose/src/encoding.rs
+++ crates/wasm-compose/src/encoding.rs
@@ -5,11 +5,8 @@ use anyhow::{anyhow, bail, Result};
 use indexmap::{IndexMap, IndexSet};
 use petgraph::EdgeDirection;
 use smallvec::SmallVec;
+use std::collections::{hash_map::Entry, HashMap};
 use std::mem;
-use std::{
-    borrow::Cow,
-    collections::{hash_map::Entry, HashMap},
-};
 use wasm_encoder::*;
 use wasmparser::{
     names::KebabString,
