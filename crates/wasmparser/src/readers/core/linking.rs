a4f35b2850f4fc5e43949043538255136edecb39
andylizi <andylizi666@gmail.com>
2024-01-03 14:27:06 -0400

    Fix copy-paste error, oops

--- crates/wasmparser/src/readers/core/linking.rs
+++ crates/wasmparser/src/readers/core/linking.rs
@@ -22,17 +22,17 @@ bitflags::bitflags! {
         const BINDING_LOCAL = 1 << 1;
         /// This is a hidden symbol.
         const VISIBILITY_HIDDEN = 1 << 2;
-        /// This symbol is intended to be exported from the wasm module to the host environment.
+        /// This symbol is not defined.
         const UNDEFINED = 1 << 4;
-        /// This symbol uses an explicit symbol name, rather than reusing the name from a wasm import.
+        /// This symbol is intended to be exported from the wasm module to the host environment.
         const EXPORTED = 1 << 5;
         /// This symbol uses an explicit symbol name, rather than reusing the name from a wasm import.
         const EXPLICIT_NAME = 1 << 6;
         /// This symbol is intended to be included in the linker output, regardless of whether it is used by the program.
         const NO_STRIP = 1 << 7;
-        /// The symbol resides in thread local storage.
+        /// This symbol resides in thread local storage.
         const TLS = 1 << 8;
-        /// The symbol represents an absolute address.
+        /// This symbol represents an absolute address.
         const ABSOLUTE = 1 << 9;
     }
 
