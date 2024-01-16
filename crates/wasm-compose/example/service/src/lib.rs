90e62051c0ffcad0301c1375fb9b482eef4c0ba4
Viktor Charypar <charypar@gmail.com>
2024-01-16 18:41:05 +0000

    update compose example (#1370)
    Co-authored-by: Stuart Harris <stuart.harris@red-badger.com>

--- crates/wasm-compose/example/service/src/lib.rs
+++ crates/wasm-compose/example/service/src/lib.rs
@@ -2,15 +2,11 @@ cargo_component_bindings::generate!();
 
 use bindings::{
     example::service::{
-        logging::{get_logger as host_get_logger, Logger as HostLogger},
+        logging::get_logger as host_get_logger,
         types::{Request, Response},
     },
-    exports::example::service::{
-        handler::{Error, Guest},
-        logging::{self, GuestLogger},
-    },
+    exports::example::service::handler::{Error, Guest},
 };
-use cargo_component_bindings::rt::Resource;
 use std::str;
 
 struct Component;
@@ -33,24 +29,13 @@ impl Guest for Component {
         let body = req.body();
         let body = str::from_utf8(&body).map_err(|_| Error::BadRequest)?;
 
+        let logger = host_get_logger();
+        logger.log(&format!("service: received request: {body}"));
+
         // Echo the body back in the response
         Ok(Response::new(
             &vec![(b"content-type".to_vec(), b"text/plain".to_vec())],
             &format!("The request body was: {body}").into_bytes(),
         ))
-    }
-}
-
-impl logging::Guest for Component {
-    fn get_logger() -> Resource<Logger> {
-        Resource::new(Logger(host_get_logger()))
-    }
-}
-
-pub struct Logger(HostLogger);
-
-impl GuestLogger for Logger {
-    fn log(&self, message: String) {
-        self.0.log(&format!("service logger: {message}"))
     }
 }
