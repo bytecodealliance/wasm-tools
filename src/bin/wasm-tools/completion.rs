cea956a8caa3a02191c7474638692ab38daf927f
mainrs <5113257+mainrs@users.noreply.github.com>
2024-01-16 20:29:26 +0100

    docs: add help message

--- src/bin/wasm-tools/completion.rs
+++ src/bin/wasm-tools/completion.rs
@@ -4,11 +4,18 @@ use clap::{CommandFactory, Parser};
 use clap_complete::{generate, Shell};
 use std::io::stdout;
 
+/// Generate shell completion scripts for this CLI.
+///
+/// Shells have different paths for their completion scripts. Please refer to their documentation.
+/// For example, to generate completions for the fish shell, run the following command below:
+///
+/// wasm-tools completion fish > ~/.config/fish/completions/wasm-tools.fish
 #[derive(Parser)]
 pub struct Opts {
     #[clap(flatten)]
     general: wasm_tools::GeneralOpts,
 
+    /// The shell to generate completions for.
     #[clap(index = 1)]
     shell: Shell,
 }
