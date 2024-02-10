ef06135b0e78a86839c6b4bbdde16a3c06889402
Alex Crichton <alex@alexcrichton.com>
2024-02-10 09:50:10 -0800

    Make anyhow non-optional for wasm-smith

--- crates/wasm-smith/tests/core.rs
+++ crates/wasm-smith/tests/core.rs
@@ -27,7 +27,7 @@ fn smoke_test_ensure_termination() {
         rng.fill_bytes(&mut buf);
         let u = Unstructured::new(&buf);
         if let Ok(mut module) = Module::arbitrary_take_rest(u) {
-            module.ensure_termination(10);
+            module.ensure_termination(10).unwrap();
             let wasm_bytes = module.to_bytes();
 
             let mut validator = Validator::new_with_features(wasm_features());
