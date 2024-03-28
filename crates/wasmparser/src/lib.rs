1ef602647666537ef3e4c2daa0129cffb72d705e
Andrew Brown <andrew.brown@intel.com>
2024-03-28 16:56:26 +0100

    Fix typo: "expection" -> "exception" (#1476)

--- crates/wasmparser/src/lib.rs
+++ crates/wasmparser/src/lib.rs
@@ -43,7 +43,7 @@
 /// The list of specializable Wasm proposals is as follows:
 ///
 /// - `@mvp`: Denoting a Wasm operator from the initial Wasm MVP version.
-/// - `@exceptions`: [Wasm `expection-handling` proposal]
+/// - `@exceptions`: [Wasm `exception-handling` proposal]
 /// - `@tail_call`: [Wasm `tail-calls` proposal]
 /// - `@reference_types`: [Wasm `reference-types` proposal]
 /// - `@sign_extension`: [Wasm `sign-extension-ops` proposal]
@@ -54,7 +54,7 @@
 /// - `@relaxed_simd`: [Wasm `relaxed-simd` proposal]
 /// - `@gc`: [Wasm `gc` proposal]
 ///
-/// [Wasm `expection-handling` proposal]:
+/// [Wasm `exception-handling` proposal]:
 /// https://github.com/WebAssembly/exception-handling
 ///
 /// [Wasm `tail-calls` proposal]:
