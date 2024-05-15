1ecfd2bd9c6e18a555147c8d0844e919ac4feecc
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-05-15 16:45:17 -0400

    Interface updates

--- crates/wit-encoder/src/interface.rs
+++ crates/wit-encoder/src/interface.rs
@@ -1,4 +1,6 @@
-use crate::{Docs, Function, Resource, Type};
+use std::fmt;
+
+use crate::{Docs, Function, Resource, ToWitSyntax, TypeDef};
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
@@ -6,14 +8,14 @@ pub struct Interface {
     /// Optionally listed name of this interface.
     ///
     /// This is `None` for inline interfaces in worlds.
-    pub name: Option<String>,
+    name: Option<String>,
 
     /// Exported types from this interface.
     ///
     /// Export names are listed within the types themselves. Note that the
     /// export name here matches the name listed in the `TypeDef`.
     #[cfg_attr(feature = "serde", serde(serialize_with = "serialize_id_map"))]
-    types: Vec<Type>,
+    type_defs: Vec<TypeDef>,
 
     /// Exported functions from this interface.
     functions: Vec<Function>,
@@ -28,10 +30,10 @@ pub struct Interface {
 
 impl Interface {
     /// Create a new instance of `Interface`.
-    pub fn new() -> Self {
+    pub fn new(name: Option<impl Into<String>>) -> Self {
         Self {
-            name: None,
-            types: vec![],
+            name: name.map(|n| n.into()),
+            type_defs: vec![],
             functions: vec![],
             resources: vec![],
             docs: Docs::default(),
@@ -39,23 +41,23 @@ impl Interface {
     }
 
     /// Add a `Type` to the interface
-    pub fn name(&mut self, name: Option<String>) {
-        self.name = name;
+    pub fn name(&mut self, name: Option<impl Into<String>>) {
+        self.name = name.map(|n| n.into());
     }
 
-    /// Add a `Type` to the interface
-    pub fn type_(&mut self, type_: Type) {
-        self.types.push(type_)
+    /// Add a type-def to the interface
+    pub fn type_def(&mut self, type_def: TypeDef) {
+        self.type_defs.push(type_def);
     }
 
     /// Add an `Function` to the interface
     pub fn function(&mut self, function: Function) {
-        self.functions.push(function)
+        self.functions.push(function);
     }
 
     /// Add a `Resource` to the interface
     pub fn resource(&mut self, resource: Resource) {
-        self.resources.push(resource)
+        self.resources.push(resource);
     }
 
     /// Set the documentation
