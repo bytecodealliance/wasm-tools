efca64729ce52dc12b85647d436ad8b12abbb6d1
MendyBerger <12537668+MendyBerger@users.noreply.github.com>
2024-05-19 00:00:35 -0400

    Added some tests

---
+++ crates/wit-encoder/tests/functions.rs
@@ -0,0 +1,70 @@
+use pretty_assertions::assert_eq;
+use wit_encoder::{Params, Result_, Results, StandaloneFunction, Type};
+
+const PACKAGE: &str = "package foo:functions;
+
+interface functions {
+    f1: func();
+    f2: func(a: u32);
+    f3: func() -> u32;
+    f4: func() -> tuple<u32, u32>;
+    f5: func(a: f32, b: f32) -> tuple<u32, u32>;
+    f6: func(a: option<u32>) -> result<u32, f32>;
+    f7: func() -> (u: u32, f: f32);
+    f8: func() -> (u: u32);
+}
+";
+
+#[test]
+fn smoke() {
+    let name = wit_encoder::PackageName::new("foo", "functions", None);
+    let mut package = wit_encoder::Package::new(name);
+
+    package.interface({
+        let mut interface = wit_encoder::Interface::new(Some("functions"));
+        interface.function(StandaloneFunction::new("f1"));
+        interface.function({
+            let mut func = StandaloneFunction::new("f2");
+            func.params(Params::from_iter([("a", Type::U32)]));
+            func
+        });
+        interface.function({
+            let mut func = StandaloneFunction::new("f3");
+            func.results(Results::anon(Type::U32));
+            func
+        });
+        interface.function({
+            let mut func = StandaloneFunction::new("f4");
+            func.results(Results::anon(Type::tuple(vec![Type::U32, Type::U32])));
+            func
+        });
+        interface.function({
+            let mut func = StandaloneFunction::new("f5");
+            func.params(Params::from_iter([("a", Type::F32), ("b", Type::F32)]));
+            func.results(Results::anon(Type::tuple(vec![Type::U32, Type::U32])));
+            func
+        });
+        interface.function({
+            let mut func = StandaloneFunction::new("f6");
+            func.params(Params::from_iter([("a", Type::option(Type::U32))]));
+            func.results(Results::anon(Type::result(Result_::both(
+                Type::U32,
+                Type::F32,
+            ))));
+            func
+        });
+        interface.function({
+            let mut func = StandaloneFunction::new("f7");
+            func.results(Results::named(vec![("u", Type::U32), ("f", Type::F32)]));
+            func
+        });
+        interface.function({
+            let mut func = StandaloneFunction::new("f8");
+            func.results(Results::named(vec![("u", Type::U32)]));
+            func
+        });
+        interface
+    });
+
+    assert_eq!(PACKAGE, package.to_string());
+}
