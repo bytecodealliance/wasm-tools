7db92a25d88f8be199aa5f0dd274272d70016f7f
Kevin Gibbons <bakkot@gmail.com>
2024-06-10 11:06:20 -0700

    color printing

--- playground/src/print.ts
+++ playground/src/print.ts
@@ -1,9 +1,14 @@
 import type { MessageToWorker } from './utilities.js';
+import type { PrintPart } from '../component-built/component.js';
+
+function unbase64(chars: string) {
+  return new Uint8Array(Array.from(atob(chars), c => c.charCodeAt(0)))
+}
 
 let overlay: HTMLDivElement = document.querySelector('#overlay')!;
 let upload: HTMLButtonElement = document.querySelector('#upload')!;
 let fileInput: HTMLInputElement = document.querySelector('#fileInput')!;
-let output: HTMLTextAreaElement = document.querySelector('#output')!;
+let output: HTMLPreElement = document.querySelector('#output')!;
 
 let lastTarget: EventTarget | null;
 let hideOverlayTimer: number;
@@ -29,13 +34,13 @@ addEventListener('drop', (e: DragEvent) => {
   overlay.style.display = 'none';
   let file = e.dataTransfer?.files[0];
   if (!file) return;
-  print(file);
+  print(file.arrayBuffer());
 }, { capture: true });
 
 let messageId = 0;
-async function print(f: File) {
-  output.value = 'working...';
-  let bytes = new Uint8Array(await f.arrayBuffer());
+async function print(f: Promise<ArrayBuffer>) {
+  output.innerHTML = 'working...';
+  let bytes = new Uint8Array(await f);
   worker.postMessage({ kind: 'print', messageId, bytes } satisfies MessageToWorker, [bytes.buffer]);
   ++messageId;
 }
@@ -46,27 +51,66 @@ let worker = new Worker('worker.js', { type: 'module' });
 // so we need to wait for it to tell us it's ready
 worker.addEventListener('message', () => {
   worker.addEventListener('message', gotMessage);
+  print(Promise.resolve(unbase64('AGFzbQEAAAABDQNgAX8Bf2ABfQBgAAACCwEDZm9vA2JhcgABAwMCAgEEBQFwAQABBQQBAQEBBwUBAWUAAQgBAQoKAgIACwUAQSoaCwsIAQBBAAsCaGk=').buffer));
 }, { once: true },);
-
-function gotMessage({ data }: {
-  data: { messageId: number; success: true; source: string } | { messageId: number; success: false; error: string };
-}) {
-  if (data.messageId !== messageId - 1) {
-    // user has already asked for a new file
-    return;
-  }
-  if (data.success) {
-    output.value = data.source;
-  } else {
-    output.value = data.error;
-  }
-}
 
 upload.addEventListener('click', () => {
   fileInput.click();
   fileInput.onchange = () => {
     let file = fileInput.files?.[0];
     if (!file) return;
-    print(file);
+    print(file.arrayBuffer());
   }
 });
+
+function gotMessage({ data }: {
+  data: { messageId: number; success: true; source: PrintPart[] } | { messageId: number; success: false; error: string };
+}) {
+  if (data.messageId !== messageId - 1) {
+    // user has already asked for a new file
+    return;
+  }
+  output.innerHTML = '';
+  if (data.success) {
+    output.append(toDOM(data.source));
+  } else {
+    let span = document.createElement('span');
+    span.style.color = 'red';
+    span.append(data.error);
+    output.append(span);
+  }
+}
+
+function toDOM(parts: PrintPart[]) {
+  let root = document.createElement('span');
+  let stack = [root];
+  for (let part of parts) {
+    switch (part.tag) {
+      case 'str': {
+        stack[0].append(part.val);
+        break;
+      }
+      case 'name':
+      case 'literal':
+      case 'keyword':
+      case 'type':
+      case 'comment': {
+        let ele = document.createElement('span');
+        ele.className = 'print-' + part.tag;
+        stack[0].append(ele);
+        stack.unshift(ele);
+        break;
+      }
+      case 'reset': {
+        stack.shift();
+        break;
+      }
+      default: {
+        // @ts-expect-error the above should be exhaustive
+        type remaining = typeof part.tag;
+        console.error('got unknown print part', part);
+      }
+    }
+  }
+  return root;
+}
