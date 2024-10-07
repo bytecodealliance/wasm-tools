e3fe7165934dc4754a799d4a7a521bdbf71661d8
Alex Crichton <alex@alexcrichton.com>
2024-10-07 12:34:15 -0700

    Gate wasm-smith component support behind a feature
    This feature is additionally off-by-default because the component
    support of `wasm-smith` has fallen quite far behind where components are
    at today.

--- crates/wasm-smith/src/core.rs
+++ crates/wasm-smith/src/core.rs
@@ -278,6 +278,7 @@ pub(crate) struct CompositeType {
 }
 
 impl CompositeType {
+    #[cfg(feature = "component-model")]
     pub(crate) fn new_func(func: Rc<FuncType>, shared: bool) -> Self {
         Self {
             inner: CompositeInnerType::Func(func),
--- crates/wasm-smith/src/lib.rs
+++ crates/wasm-smith/src/lib.rs
@@ -50,16 +50,19 @@
 //! The design and implementation strategy of wasm-smith is outlined in
 //! [this article](https://fitzgeraldnick.com/2020/08/24/writing-a-test-case-generator.html).
 
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![deny(missing_docs, missing_debug_implementations)]
 // Needed for the `instructions!` macro in `src/code_builder.rs`.
 #![recursion_limit = "512"]
 
+#[cfg(feature = "component-model")]
 mod component;
 mod config;
 mod core;
 
 pub use crate::core::{InstructionKind, InstructionKinds, Module};
 use arbitrary::{Result, Unstructured};
+#[cfg(feature = "component-model")]
 pub use component::Component;
 pub use config::{Config, MemoryOffsetChoices};
 use std::{collections::HashSet, fmt::Write, str};
@@ -137,6 +140,7 @@ pub(crate) fn unique_string(
     Ok(name)
 }
 
+#[cfg(feature = "component-model")]
 pub(crate) fn unique_kebab_string(
     max_size: usize,
     names: &mut HashSet<String>,
@@ -185,6 +189,7 @@ pub(crate) fn unique_kebab_string(
     Ok(name)
 }
 
+#[cfg(feature = "component-model")]
 pub(crate) fn unique_url(
     max_size: usize,
     names: &mut HashSet<String>,
