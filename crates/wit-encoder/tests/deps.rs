c957ab6dee30eab877e33a754c655038ac3ed2e0
Dávid István Bíró <noise64@gmail.com>
2024-10-22 09:48:35 +0200

    test cleanup and add check for resolved-encoded output

--- crates/wit-encoder/tests/deps.rs
+++ crates/wit-encoder/tests/deps.rs
@@ -3,34 +3,73 @@ use pretty_assertions::assert_eq;
 use wit_encoder::{packages_from_parsed, Package};
 use wit_parser::Resolve;
 
-const MAIN_PACKAGE_CASE_1: &str = indoc::indoc! {"
+const MAIN_PACKAGE_SOURCE: &str = indoc::indoc! {"
     package foo:main;
 
     interface main-interface-a {
-        use foo:dep-a/dep-a-interface-b.{ra};
+      use foo:dep-a/dep-a-interface-b.{ ra };
     }
 
-    interface main-interface-b {
-    }
+    interface main-interface-b {}
 
-    interface main-interface-c {
-    }
+    interface main-interface-c {}
+
+    interface main-interface-d {}
+
+    interface main-interface-e {}
 
     world main-world-a {
-        import foo:dep-c/dep-c-interface-a;
-        export foo:dep-c/dep-c-interface-b;
-        include foo:dep-c/dep-c-world-a;
+      import foo:dep-c/dep-c-interface-a;
+      export foo:dep-c/dep-c-interface-b;
+      include foo:dep-c/dep-c-world-a;
 
-        import foo:dep-b/dep-b-interface-a@1.2.3;
-        export foo:dep-b/dep-b-interface-b@1.2.3;
-        include foo:dep-b/dep-b-world-b@1.2.3;
+      import foo:dep-b/dep-b-interface-a@1.2.3;
+      export foo:dep-b/dep-b-interface-b@1.2.3;
+      include foo:dep-b/dep-b-world-b@1.2.3;
 
-        import main-interface-b;
-        export main-interface-a;
-        include main-world-b;
+      import main-interface-b;
+      export main-interface-a;
+      include main-world-b;
     }
 
     world main-world-b {
+      export main-interface-d;
+      import main-interface-d;
+    }
+"};
+
+const MAIN_PACKAGE_RESOLVED_ENCODED: &str = indoc::indoc! {"
+    package foo:main;
+
+    interface main-interface-a {
+      use foo:dep-a/dep-a-interface-b.{ ra };
+    }
+
+    interface main-interface-b {}
+
+    interface main-interface-c {}
+
+    interface main-interface-d {}
+
+    interface main-interface-e {}
+
+    world main-world-b {
+      import main-interface-d;
+      export main-interface-d;
+    }
+
+    world main-world-a {
+      import foo:dep-c/dep-c-interface-a;
+      import foo:dep-b/dep-b-interface-a@1.2.3;
+      import main-interface-b;
+      import foo:dep-c/dep-c-interface-c;
+      import main-interface-d;
+      import foo:dep-a/dep-a-interface-b;
+      export foo:dep-c/dep-c-interface-b;
+      export foo:dep-b/dep-b-interface-b@1.2.3;
+      export main-interface-a;
+      export foo:dep-c/dep-c-interface-d;
+      export main-interface-d;
     }
 "};
 
@@ -38,58 +77,53 @@ const DEP_PACKAGE_A: &str = indoc::indoc! {"
     package foo:dep-a;
 
     interface dep-a-interface-b {
-        use foo:dep-b/dep-b-interface-a@1.2.3.{a};
-        type ra = a;
+      use foo:dep-b/dep-b-interface-a@1.2.3.{a};
+      type ra = a;
     }
 
-    world dep-a-world {
-    }
+    world dep-a-world {}
 "};
 
 const DEP_PACKAGE_B: &str = indoc::indoc! {"
     package foo:dep-b@1.2.3;
 
     interface dep-b-interface-a {
-        record a {
-            x: f32,
-            y: f32,
-        }
+      record a {
+        x: f32,
+        y: f32,
+      }
     }
 
     interface dep-b-interface-b {
-        use foo:dep-c/dep-c-interface-a.{a};
+      use foo:dep-c/dep-c-interface-a.{a};
     }
 
     world dep-b-world-a {
-        export dep-b-interface-a;
+      export dep-b-interface-a;
     }
 
-    world dep-b-world-b {
-    }
+    world dep-b-world-b {}
 "};
 
 const DEP_PACKAGE_C: &str = indoc::indoc! {"
     package foo:dep-c;
 
     interface dep-c-interface-a {
-        record a {
-            x: f32,
-            y: f32,
-        }
+      record a {
+        x: f32,
+        y: f32,
+      }
     }
 
-    interface dep-c-interface-b {
-    }
+    interface dep-c-interface-b {}
 
-    interface dep-c-interface-c {
-    }
+    interface dep-c-interface-c {}
 
-    interface dep-c-interface-d {
-    }
+    interface dep-c-interface-d {}
 
     world dep-c-world-a {
-        import dep-c-interface-c;
-        export dep-c-interface-d;
+      import dep-c-interface-c;
+      export dep-c-interface-d;
     }
 "};
 
@@ -100,7 +134,7 @@ fn resolve_encode_resolve_round_trip_with_use_and_include() {
             ("dep_c.wit", DEP_PACKAGE_C),
             ("dep_b.wit", DEP_PACKAGE_B),
             ("dep_a.wit", DEP_PACKAGE_A),
-            ("main_foo.wit", MAIN_PACKAGE_CASE_1),
+            ("main_foo.wit", MAIN_PACKAGE_SOURCE),
         ])
         .unwrap(),
     );
@@ -128,6 +162,10 @@ fn resolve_encode_resolve_round_trip_with_use_and_include() {
         &packages,
         &["foo:dep-c", "foo:dep-b@1.2.3", "foo:dep-a", "foo:main"],
     );
+
+
+    // Check for the encoded main wit
+    assert_eq!(MAIN_PACKAGE_RESOLVED_ENCODED, &packages[3].to_string());
 }
 
 fn resolve_packages(packages: &[(&str, &str)]) -> Result<Resolve> {
