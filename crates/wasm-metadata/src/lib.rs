308733d478a9dfe0773ff967c3d63c20619a2914
Yosh <github@yosh.is>
2024-11-28 14:57:32 +0100

    move module_names

--- crates/wasm-metadata/src/lib.rs
+++ crates/wasm-metadata/src/lib.rs
@@ -6,13 +6,15 @@ use std::mem;
 use std::ops::Range;
 use wasm_encoder::ComponentSection as _;
 use wasm_encoder::{ComponentSectionId, Encode, Section};
-use wasmparser::{BinaryReader, KnownCustom, NameSectionReader, Parser, Payload::*};
+use wasmparser::{KnownCustom, Parser, Payload::*};
 
 pub use component_name::ComponentNames;
+pub use module_name::ModuleNames;
 pub use producers::{Producers, ProducersField};
 pub use registry::{CustomLicense, Link, LinkType, RegistryMetadata};
 
 mod component_name;
+mod module_name;
 mod producers;
 mod registry;
 
@@ -406,92 +408,6 @@ impl Metadata {
 impl fmt::Display for Metadata {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         self.display(f, 0)
-    }
-}
-
-/// Helper for rewriting a module's name section with a new module name.
-pub struct ModuleNames<'a> {
-    module_name: Option<String>,
-    names: Vec<wasmparser::Name<'a>>,
-}
-
-impl<'a> ModuleNames<'a> {
-    /// Create an empty name section.
-    pub fn empty() -> Self {
-        ModuleNames {
-            module_name: None,
-            names: Vec::new(),
-        }
-    }
-    /// Read a name section from a WebAssembly binary. Records the module name, and all other
-    /// contents of name section, for later serialization.
-    pub fn from_bytes(bytes: &'a [u8], offset: usize) -> Result<ModuleNames<'a>> {
-        let reader = BinaryReader::new(bytes, offset);
-        let section = NameSectionReader::new(reader);
-        let mut s = Self::empty();
-        for name in section.into_iter() {
-            let name = name?;
-            match name {
-                wasmparser::Name::Module { name, .. } => s.module_name = Some(name.to_owned()),
-                _ => s.names.push(name),
-            }
-        }
-        Ok(s)
-    }
-    /// Update module section according to [`AddMetadata`]
-    fn from_name(name: &Option<String>) -> Self {
-        let mut s = Self::empty();
-        s.module_name = name.clone();
-        s
-    }
-
-    /// Merge with another section
-    fn merge(&mut self, other: &Self) {
-        if other.module_name.is_some() {
-            self.module_name = other.module_name.clone();
-        }
-        self.names.extend_from_slice(&other.names);
-    }
-
-    /// Set module name
-    pub fn set_name(&mut self, name: &str) {
-        self.module_name = Some(name.to_owned())
-    }
-    /// Get module name
-    pub fn get_name(&self) -> Option<&String> {
-        self.module_name.as_ref()
-    }
-    /// Serialize into [`wasm_encoder::NameSection`].
-    fn section(&self) -> Result<wasm_encoder::NameSection> {
-        let mut section = wasm_encoder::NameSection::new();
-        if let Some(module_name) = &self.module_name {
-            section.module(&module_name);
-        }
-        for n in self.names.iter() {
-            match n {
-                wasmparser::Name::Module { .. } => unreachable!(),
-                wasmparser::Name::Function(m) => section.functions(&name_map(&m)?),
-                wasmparser::Name::Local(m) => section.locals(&indirect_name_map(&m)?),
-                wasmparser::Name::Label(m) => section.labels(&indirect_name_map(&m)?),
-                wasmparser::Name::Type(m) => section.types(&name_map(&m)?),
-                wasmparser::Name::Table(m) => section.tables(&name_map(&m)?),
-                wasmparser::Name::Memory(m) => section.memories(&name_map(&m)?),
-                wasmparser::Name::Global(m) => section.globals(&name_map(&m)?),
-                wasmparser::Name::Element(m) => section.elements(&name_map(&m)?),
-                wasmparser::Name::Data(m) => section.data(&name_map(&m)?),
-                wasmparser::Name::Field(m) => section.fields(&indirect_name_map(&m)?),
-                wasmparser::Name::Tag(m) => section.tags(&name_map(&m)?),
-                wasmparser::Name::Unknown { .. } => {} // wasm-encoder doesn't support it
-            }
-        }
-        Ok(section)
-    }
-
-    /// Serialize into the raw bytes of a wasm custom section.
-    pub fn raw_custom_section(&self) -> Result<Vec<u8>> {
-        let mut ret = Vec::new();
-        self.section()?.encode(&mut ret);
-        Ok(ret)
     }
 }
 
