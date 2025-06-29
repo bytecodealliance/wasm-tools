fa82d0b75cf3de2458abd629f90f214f0ea2ceb6
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2025-06-29 12:16:48 -0400

    Fix spelling wit_encoder::RenderOpts.ident_count to indent_count

--- crates/wit-encoder/src/render.rs
+++ crates/wit-encoder/src/render.rs
@@ -7,14 +7,14 @@ pub struct RenderOpts {
     /// width of each indent
     pub indent_width: usize,
     /// current indent depth
-    pub ident_count: usize,
+    pub indent_count: usize,
 }
 
 impl Default for RenderOpts {
     fn default() -> Self {
         Self {
             indent_width: 2,
-            ident_count: 0,
+            indent_count: 0,
         }
     }
 }
@@ -22,27 +22,27 @@ impl Default for RenderOpts {
 impl RenderOpts {
     /// Indent
     ///
-    /// This will clone Self, and increment self.ident_count.
+    /// This will clone Self, and increment self.indent_count.
     pub fn indent(&self) -> Self {
         Self {
             indent_width: self.indent_width,
-            ident_count: self.ident_count + 1,
+            indent_count: self.indent_count + 1,
         }
     }
 
     /// Outdent
     ///
-    /// This will clone Self, and decrement self.ident_count.
+    /// This will clone Self, and decrement self.indent_count.
     pub fn outdent(&self) -> Self {
         Self {
             indent_width: self.indent_width,
-            ident_count: self.ident_count - 1,
+            indent_count: self.indent_count - 1,
         }
     }
 
     /// Get the actual characters
     pub fn spaces(&self) -> String {
-        let space_count = self.indent_width * self.ident_count;
+        let space_count = self.indent_width * self.indent_count;
         " ".repeat(space_count)
     }
 }
