9186e9722147fa12629de9152d5253ff53918c3e
Tim Chevalier <tjc@igalia.com>
2025-09-30 11:45:50 -0700

    Expand --help output for `parse` and `print` commands (#2320)
    Add example usage sections to the --help output for these commands.

--- src/bin/wasm-tools/parse.rs
+++ src/bin/wasm-tools/parse.rs
@@ -4,8 +4,29 @@ use clap::Parser;
 /// Parse the WebAssembly text format.
 ///
 /// This subcommand will parse the provided input as the WebAssembly text format
-/// and optionally write the binary form to a provided file.
+/// and optionally write the binary or text form to a provided file.
 #[derive(Parser)]
+#[clap(after_help = "
+Examples:
+
+   To parse the file input.wat as WebAssembly text format and write the
+   normalized text format to stdout:
+
+          wasm-tools parse -t input.wat
+
+   To parse the file input.wat as WebAssembly text format and write the binary
+   form to the file output.wasm, including debugging information:
+
+          wasm-tools parse input.wat -g -o output.wasm
+
+   To parse the file input.wat as WebAssembly text format and write the
+   normalized text format to the file output.wat:
+
+          wasm-tools parse -t input.wat -o output.wat
+
+Exit status:
+    0 if OK,
+    nonzero to indicate a parse error.")]
 pub struct Opts {
     #[clap(flatten)]
     generate_dwarf: wasm_tools::GenerateDwarfArg,
