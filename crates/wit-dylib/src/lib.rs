92db82b262c33bd1feed7f386c667a464057b730
Yordis Prieto <yordis.prieto@gmail.com>
2025-11-21 14:56:05 -0500

    refactor(wit): revert map type support from the dylib

--- crates/wit-dylib/src/lib.rs
+++ crates/wit-dylib/src/lib.rs
@@ -571,16 +571,6 @@ impl Adapter {
                 });
                 metadata::Type::List(index)
             }
-            TypeDefKind::Map(key_ty, value_ty) => {
-                let index = self.metadata.maps.len();
-                self.metadata.maps.push(metadata::Map {
-                    interface,
-                    name,
-                    key_ty: self.lookup_ty(key_ty),
-                    value_ty: self.lookup_ty(value_ty),
-                });
-                metadata::Type::Map(index)
-            }
             TypeDefKind::FixedSizeList(t, len) => {
                 let index = self.metadata.fixed_size_lists.len();
                 self.metadata
@@ -629,6 +619,9 @@ impl Adapter {
             }
             TypeDefKind::Handle(Handle::Borrow(t)) => {
                 metadata::Type::Borrow(self.resource_map[&dealias(resolve, *t)])
+            }
+            TypeDefKind::Map(_, _) => {
+                todo!("map")
             }
             TypeDefKind::Unknown => unreachable!(),
         };
