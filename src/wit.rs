1050e4a6b4f672534771b987be96d4609c885ac9
Phoebe Szmucer <129869709+PhoebeSzmucer@users.noreply.github.com>
2026-05-04 10:36:08 +0100

    fmt

--- src/wit.rs
+++ src/wit.rs
@@ -43,7 +43,10 @@ impl WitResolve {
     pub fn load(&self) -> Result<(Resolve, PackageId)> {
         let mut resolve = Self::resolve_with_features(&self.features, self.all_features);
         let (pkg_id, _) = resolve.push_path(&self.wit).map_err(|e| {
-            anyhow::anyhow!("{}", wit_parser::render_anyhow_error(&e, &resolve.source_map))
+            anyhow::anyhow!(
+                "{}",
+                wit_parser::render_anyhow_error(&e, &resolve.source_map)
+            )
         })?;
         Ok((resolve, pkg_id))
     }
