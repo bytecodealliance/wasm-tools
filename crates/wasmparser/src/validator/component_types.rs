8208701f098a7b762a05e9172bedac659681486f
Christof Petig <christof.petig@arcor.de>
2026-09-13 17:55:56 +0200

    correct pointer and memory logic

--- crates/wasmparser/src/validator/component_types.rs
+++ crates/wasmparser/src/validator/component_types.rs
@@ -1848,7 +1848,7 @@ impl ComponentDefinedType {
                 .cases
                 .values()
                 .any(|case| case.ty.map(|ty| ty.contains_ptr(types)).unwrap_or(false)),
-            Self::List { .. } | Self::Map { .. } => true,
+            Self::List { .. } | Self::Map { .. } | Self::FixedLengthList { .. } => true,
             Self::Tuple(t) => t.types.iter().any(|ty| ty.contains_ptr(types)),
             Self::Flags(_)
             | Self::Enum(_)
@@ -1856,9 +1856,7 @@ impl ComponentDefinedType {
             | Self::Borrow(_)
             | Self::Future { .. }
             | Self::Stream { .. } => false,
-            Self::Option { ty, .. } | Self::FixedLengthList { element: ty, .. } => {
-                ty.contains_ptr(types)
-            }
+            Self::Option { ty, .. } => ty.contains_ptr(types),
             Self::Result { ok, err, .. } => {
                 ok.map(|ty| ty.contains_ptr(types)).unwrap_or(false)
                     || err.map(|ty| ty.contains_ptr(types)).unwrap_or(false)
